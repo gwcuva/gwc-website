0771ca8c54c5e0afa8dabd5fefc03f7f3b4847bf
Mara Hart <maralihart@gmail.com>
2020-09-24 22:59:09 -0700

    moOoOooOoOOre SSL testing

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from django.views.generic.base import TemplateView
 
 app_name ='app'
 
@@ -15,4 +16,8 @@ urlpatterns = [
     path('meetTheTeam', views.meetTheTeam, name = 'meetTheTeam'),
     path('join', views.join, name = 'join'),
     path('lessonPage', views.lessonPage, name = 'lessonPage'),
+    path(
+        '.well-known/pki-validation/26135DB70D3412BDE5E30A182550DCDD.txt',
+        TemplateView.as_view(template_name="26135DB70D3412BDE5E30A182550DCDD.txt", content_type="text/plain"),
+    ),
 ]
