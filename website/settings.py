911be5ca203874b32beb36feec54dbc820741186
Mara Hart <maralihart@gmail.com>
2020-10-01 11:46:31 -0400

    Changed settings to force SSL

--- website/settings.py
+++ website/settings.py
@@ -53,6 +53,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 ROOT_URLCONF = 'website.urls'
 
 TEMPLATES = [
