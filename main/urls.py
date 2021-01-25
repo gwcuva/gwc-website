fe21d8c9765e42db66bd6e7baad29594780f543c
Jessie Eoff <jessieeoff2@gmail.com>
2021-01-25 07:23:09 -0600

    Clean up

--- main/urls.py
+++ main/urls.py
@@ -6,12 +6,12 @@ from django.views.generic.base import TemplateView
 app_name ='app'
 
 urlpatterns = [
-    path('hackathon', views.main, name = 'main'),
+    path('', views.main, name = 'main'),
     path('home', views.home, name = 'home'),
     path('lesson1', views.lesson1, name = 'lesson1'),
     path('lesson2', views.lesson2, name = 'lesson2'),
     path('lesson3', views.lesson3, name = 'lesson3'),
-    path('', views.hackathon, name = 'hackathon'),
+    path('hackathon', views.hackathon, name = 'hackathon'),
     path('calendar', views.calendar, name = 'calendar'),
     path('meetTheTeam', views.meetTheTeam, name = 'meetTheTeam'),
     path('join', views.join, name = 'join'),
