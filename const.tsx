c4a393118dfc24757a16fc3e4a7eb28556d1ff6a
Mara Hart <maralihart@gmail.com>
2021-03-05 17:38:36 -0800

    changed color names

--- const.tsx
+++ const.tsx
@@ -1,9 +1,10 @@
-enum color {
-  'aqua' = '#43D6B9',
-  'bright blue' = '#0D38D3',
-  'navy' = '#003046',
+export enum color {
+  'turquoise' = '#43D6B9',
+  'blue' = '#0D38D3',
+  'peacock' = '#003046',
   'orange' = '#FA7815',
-  'white' = '#EDEDED',
+  'white' = '#FFFFFF',
+  'light grey' = '#EDEDED',
   'peach' = '#F9B88C',
   'grey' = '#CECECE',
   'dark grey' = '#7A7A78',
