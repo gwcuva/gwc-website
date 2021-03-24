337cab84c088a441b78176b319a41640fc1d0a42
Mara Hart <maralihart@gmail.com>
2021-03-23 20:32:28 -0400

    adding classes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import Navigation from './Navigation';
 
+const StyledLink = styled.a`
+  color: white;
+  text-decoration: none;
+  font-family: Roboto Mono;
+  font-size: 16pt;
+`
+
 function Navbar() {
   return (
     <Navigation>
       {/* TODO: Make this image work, also replace with full-navy image */}
       <img src="../../assets/logo-black-white-horizontal.png" alt="Girls Who Code at the University of Virginia logo"/>
-      <a href="#" target="_blank" class="mono">Girls Hoo Hack</a>
+      <StyledLink href="#" target="_blank" class="mono text-white">Girls Hoo Hack</StyledLink>
     </Navigation>
   );
 }
