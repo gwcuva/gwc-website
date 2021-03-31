c250808558c7edd5a1cfa0498fad12819e2b7ae1
Mara Hart <maralihart@gmail.com>
2021-03-31 19:51:29 -0400

    added all components to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
+import Stats from './components/Stats/Stats';
+import DiscordInvite from './components/DiscordInvite/DiscordInvite';
+import Team from './components/Team/Team';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
+      <Stats />
+      <About />
+      <DiscordInvite />
+      <Team />
+      <Footer />
     </div>
   );
 }
