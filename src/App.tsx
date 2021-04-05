f36244050ca495a4208fdb804cc8ca88a50474c4
Mara Hart <maralihart@gmail.com>
2021-04-05 13:39:25 -0400

    categorized main

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import Header from './components/Header/Header';
-import Stats from './components/Stats/Stats';
-import DiscordInvite from './components/DiscordInvite/DiscordInvite';
-import Team from './components/Team/Team';
-import About from './components/About/About';
-import Footer from './components/Footer/Footer';
+import Navbar from './components/Main/Navbar/Navbar';
+import Header from './components/Main/Header/Header';
+import Stats from './components/Main/Stats/Stats';
+import DiscordInvite from './components/Main/DiscordInvite/DiscordInvite';
+import Team from './components/Main/Team/Team';
+import About from './components/Main/About/About';
+import Footer from './components/Main/Footer/Footer';
 
 function App() {
   return (
