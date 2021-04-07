26e05be23d1ade8255d7d9bc32cfec7342b9e1a6
pragnya17 <pp8sdv@virginia.edu>
2021-04-07 04:58:38 -0400

    Aligning text in header

--- src/components/Main/Header/Header.tsx
+++ src/components/Main/Header/Header.tsx
@@ -5,11 +5,39 @@ import React from 'react';
 
 function Header() {
   return (
-    <div>
-      <h2>HELLO WORLD</h2>
-      <h1>WE'RE MAKING WAVES</h1>
-      <h2>GWC at UVA</h2>
+    <div className="container">
+      <div className="row justify-content-start">
+        <div className="col-12">
+          <h5>HELLO WORLD</h5>
+        </div>
+      </div>
+
+      <div className="row justify-content-center">
+        <div className="col-10">
+          <h1>WE'RE</h1>
+        </div>
+      </div>
+
+      <div className="row justify-content-end">
+        <div className="col-10">
+          <h1>MAKING</h1>
+        </div>
+      </div>
+
+      <div className="row justify-content-center">
+        <div className="col-10">
+          <h1>WAVES</h1>
+        </div>
+      </div>
+
+      <div className="row justify-content-end">
+        <div className="col-8">
+          <h5>GWC AT UVA</h5>
+        </div>
+      </div>
+
       <img src="../../assets/images/main-header-wave.svg" alt="Orange wave" />
+
     </div>
   );
 }
