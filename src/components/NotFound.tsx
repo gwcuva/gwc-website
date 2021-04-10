97efb80b219ad23c86a08e896d97b4f5c3e7cf7b
belletran <71110016+bellet6@users.noreply.github.com>
2021-04-10 12:24:35 -0400

    changed font sizes for 404 page
    added h2 tags to main page and hackathon page link

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -4,8 +4,8 @@ function NotFound() {
   return (
     <div>
       <h1 className="text-peacock">Page Not Found</h1>
-      <a href="/" className="text-orange">Go to Main Page</a>
-      <a href="/" className="text-orange">Go to Hackathon Page</a>
+      <h2 className="text-orange"><a href="/">Go to Main Page</a></h2>
+      <h2 className="text-orange"><a href="/">Go to Hackathon Page</a></h2>
     </div>
   );
 }
