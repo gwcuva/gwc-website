8ecf94974e477c1b598eda0778b23db1666c1626
mayatietz <mayatietz@gmail.com>
2021-04-10 00:52:20 -0400

    add NavCircle to Main.tsx

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -6,19 +6,19 @@ import DiscordInvite from './DiscordInvite/DiscordInvite';
 import Team from './Team/Team';
 import About from './About/About';
 import Footer from './Footer/Footer';
-import NavCircle from './NavCirle/NavCircle';
+import NavCircle from './NavCircle/NavCircle';
 
 function Main() {
   return (
     <div>
       <Navbar />
-      <NavCircle />
       <Header />
       <Stats />
       <About />
       <DiscordInvite />
       <Team />
       <Footer />
+      <NavCircle />
     </div>
   );
 }
