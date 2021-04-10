57558df6dc2a2c482ccf5019920981bd7d2fe543
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-10 14:39:04 -0400

    add link to discord

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -7,7 +7,7 @@ function DiscordInvite() {
   return (
     <div>
       <h4>Want to join Girls Who Code at UVA?</h4>
-      <a href="#" target="_blank"><h2>Join our Discord server ---{">"}</h2></a>
+      <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2>Join our Discord server ---{">"}</h2></a>
       <img src="get started.svg" alt="Orange wave saying 'Get Started'"/>
     </div>
   );
