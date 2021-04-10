3cc8689613db291c9bc52130268494c6a0ee8289
Jennifer <jt4nb@virginia.edu>
2021-04-10 01:04:14 -0400

    added social links + svgs and changed column sizes

--- src/components/Main/Footer/Contact.tsx
+++ src/components/Main/Footer/Contact.tsx
@@ -10,7 +10,14 @@ function Contact() {
     <div>
       <h5>Contact</h5>
       <p>gwcuva@gmail.com</p>
-      <img src="#" alt="icon" />
+      <div className="row">
+        <a href="https://www.instagram.com/gwcuva/" target="_blank"><img src="/src/assets/images/instagram-icon.svg" alt="Instagram icon"/></a>
+        <a href="https://discord.com/invite/rKbWyuDNyG" target="_blank"><img src="/src/assets/images/discord-icon.svg" alt="Discord icon"/></a>
+      </div>
+      <div className="row">
+        <a href="https://www.facebook.com/gwcuva/" target="_blank"><img src="/src/assets/images/facebook-icon.svg" alt="Facebook icon"/></a>
+        <a href="https://twitter.com/gwcuva?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank"><img src="/src/assets/images/twitter-icon.svg" alt="Twitter icon"/></a>
+      </div>
     </div>
   );
 }
--- src/components/Main/Footer/Footer.tsx
+++ src/components/Main/Footer/Footer.tsx
@@ -9,19 +9,20 @@ import Links from './Links';
 // TODO: Add links (Brand Manual should redirect to Figma since we haven't coded it)
 
 const FooterSection =  styled.section`
-  background-image: url(/src/assets/images/main-header-wave.svg)
+  background-image: url(/src/assets/images/main-header-wave.svg);
 `;
+
 function Footer() {
   return (
     <FooterSection>
-      <div className="row">
-        <div className="col">
-          <img src="/src/assets/images/logo-black-white-horizontal.png" alt="Girls Who Code at the University of Virginia logo" />
+      <div className="row ml-2">
+        <div className="col-4">
+          <img src="/src/assets/images/logo-black-white-horizontal.png" alt="Girls Who Code at the University of Virginia logo"/>
         </div>
-        <div className="col">
+        <div className="col-2">
           <Contact />
         </div>
-        <div className="col">
+        <div className="col-2">
           <Links />
         </div>
       </div>
