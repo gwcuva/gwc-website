1f2cce5991a7299c94032b566622a64279138aba
belletran <71110016+bellet6@users.noreply.github.com>
2021-04-10 11:25:49 -0400

    fixed syntax errors in NotFound.tsx

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 function NotFound() {
   return (
     <div>
-      <h1 class="text-peacock">Page Not Found</h1>
-      <a href="/" class="text-orange">Go to Main Page</a>
-      <a href="/" class="text-orange">Go to Hackathon Page</a>
+      <h1 className="text-peacock">Page Not Found</h1>
+      <a href="/" className="text-orange">Go to Main Page</a>
+      <a href="/" className="text-orange">Go to Hackathon Page</a>
     </div>
   );
 }
