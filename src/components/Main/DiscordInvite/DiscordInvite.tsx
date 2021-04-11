6dd3cbf8f651f8339547beb24898aad3d923452d
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-11 13:03:08 -0400

    screen size adjustments

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -6,11 +6,16 @@ import './discordstyle.css';
 function DiscordInvite() {
   return (
     <div>
-      <h4 className="text-turq join"><b>Want to join Girls Who Code at UVA?</b></h4>
-      <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord"><b>Join our Discord <br/>server ---{">"}</b></h2></a>
-      <img src="get started.svg" alt="Orange wave saying 'Get Started'" />
+      <div className="row">
+
+        <div className="col-lg-12 col-md-12 col-sm-12">
+          <h4 className="text-turq join"><b>Want to join Girls Who Code at UVA?</b></h4>
+          <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord"><b>Join our Discord <br />server ---{">"}</b></h2></a>
+          <img src="get started.svg" alt="Orange wave saying 'Get Started'" />
+        </div>
+      </div>
     </div>
-    );
+  );
 }
 
 export default DiscordInvite;
