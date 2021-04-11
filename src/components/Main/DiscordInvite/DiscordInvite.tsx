666a77a000f059c04a877dc5eabf9ec31f57aa8c
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-10 21:50:44 -0400

    add image

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { ReactSVG } from 'react-svg';
 
 // TODO: Add text and link
 // TODO: Add graphic at the end of the page
@@ -8,9 +10,11 @@ function DiscordInvite() {
     <div>
       <h4 className="text-turq text-center"><b>Want to join Girls Who Code at UVA?</b></h4>
       <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock text-center"><b>Join our Discord <br/>server ---{">"}</b></h2></a>
-      <img src="get started.svg" alt="Orange wave saying 'Get Started'"/>
+      <img src="get started.svg" alt="Orange wave saying 'Get Started'" />
     </div>
-  );
+    );
 }
+// render(<ReactSVG src="get started.svg" />, document.getElementById('image'))
+
 
 export default DiscordInvite;
