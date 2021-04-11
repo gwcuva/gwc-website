debef9688f6b76a363e2b826b1262ecfb8573da4
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-10 21:28:25 -0400

    fix alignment

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -6,8 +6,8 @@ import React from 'react';
 function DiscordInvite() {
   return (
     <div>
-      <h4 className="text-turq"><b>Want to join Girls Who Code at UVA?</b></h4>
-      <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord-size"><b>Join our Discord <br/>server ---{">"}</b></h2></a>
+      <h4 className="text-turq text-center"><b>Want to join Girls Who Code at UVA?</b></h4>
+      <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock text-center"><b>Join our Discord <br/>server ---{">"}</b></h2></a>
       <img src="get started.svg" alt="Orange wave saying 'Get Started'"/>
     </div>
   );
