2855a3c95c3e34c154406011e95c4e47c632e8d3
Mara Hart <42477471+maralihart@users.noreply.github.com>
2021-04-11 19:09:01 -0400

    Mlh4xc/figma font sync (#61)
    * updated tags to fit figma
    
    * desktop h1,2,3 edited
    
    * changed desktop text elements
    
    * added mobile font sizes

--- src/components/Main/Stats/Stats.tsx
+++ src/components/Main/Stats/Stats.tsx
@@ -8,11 +8,11 @@ import Percentage from './Percentage';
 function Stats() {
   return (
     <div>
-      <h3>Let's reprograming the tech world.</h3>
+      <h2>Let's reprograming the tech world.</h2>
       <Percentage top="1995" percent="37%" fact="of programmers are women"/>
       <Percentage top="2020" percent="24%" fact="of programmers are women"/>
 
-      <h4>Together, we can close the gender gap in CS.</h4>
+      <h3>Together, we can close the gender gap in CS.</h3>
       <CaptionedGraphic img="#" alt="#" caption="Filler" />
       <CaptionedGraphic img="#" alt="#" caption="Filler" />
       <CaptionedGraphic img="#" alt="#" caption="Filler" />
