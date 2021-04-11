0a45e5f23196a078c06b94e9953e38fe1b215bf2
mayatietz <mayatietz@gmail.com>
2021-04-10 22:24:24 -0400

    add social media links
    instagram and discord

--- src/components/Main/NavCircle/NavCircle.tsx
+++ src/components/Main/NavCircle/NavCircle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // TODO: Add circle background
 // TODO: Add three links
-// TODO: Add social media links
+// TODO: Add social media links (done)
 // TODO: (if needed in a future PR) depending on scroll position, change the color of circle border
 // TODO: Make absolute on the page so it follows the screen
 
@@ -12,8 +12,8 @@ function NavCircle() {
       <a href="#" target="_blank">the gap</a>
       <a href="#" target="_blank">the mission</a>
       <a href="#" target="_blank">the team</a>
-      <a href="#" target="_blank"><img src="#" alt="Instagram" /></a>
-      <a href="#" target="_blank"><img src="#" alt="Discord" /></a>
+      <a href="https://www.instagram.com/gwcuva/?hl=en" target="_blank"><img src="#" alt="Instagram" /></a>
+      <a href="https://discord.gg/PmER5Fvp" target="_blank"><img src="#" alt="Discord" /></a>
     </div>
   );
 }
