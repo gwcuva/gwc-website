03543737d1d36d4b25d1e5399ea472e7533089ca
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-11 12:56:07 -0400

    fix positioning

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-
+import './discordstyle.css';
 // TODO: Add text and link
 // TODO: Add graphic at the end of the page
 
 function DiscordInvite() {
   return (
     <div>
-      <h4 className="text-turq text-center"><b>Want to join Girls Who Code at UVA?</b></h4>
-      <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock text-center"><b>Join our Discord <br/>server ---{">"}</b></h2></a>
+      <h4 className="text-turq join"><b>Want to join Girls Who Code at UVA?</b></h4>
+      <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord"><b>Join our Discord <br/>server ---{">"}</b></h2></a>
       <img src="get started.svg" alt="Orange wave saying 'Get Started'" />
     </div>
     );
 }
-// render(<ReactSVG src="get started.svg" />, document.getElementById('image'))
-
 
 export default DiscordInvite;
