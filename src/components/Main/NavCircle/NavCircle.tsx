8f3e7f7ca23c810f27f4f9ae1696df6fabaf5157
mayatietz <mayatietz@gmail.com>
2021-04-11 21:42:01 -0400

    added circle to page

--- src/components/Main/NavCircle/NavCircle.tsx
+++ src/components/Main/NavCircle/NavCircle.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
 
 // TODO: Add circle background
-// TODO: Add three links
+// TODO: Add three links 
 // TODO: Add social media links (done)
 // TODO: (if needed in a future PR) depending on scroll position, change the color of circle border
 // TODO: Make absolute on the page so it follows the screen
 
+
+const Circle = () => {
+  return (
+<svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={303}
+      height={303}
+      viewBox="0 0 303 303"
+      overflow="visible"
+    >
+      <circle
+        cx={151.5}
+        cy={151.5}
+        r={150}
+        fill="none"
+        stroke="#43d6b9"
+        strokeWidth={3}
+        strokeMiterlimit={10}
+      />
+    </svg>
+
+  )
+}
+
 function NavCircle() {
   return (
+
     <div>
+      <Circle />
       <a href="#" target="_blank">the gap</a>
       <a href="#" target="_blank">the mission</a>
       <a href="#" target="_blank">the team</a>
