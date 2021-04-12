789c72744ec0e0a5e892eada6d1dc502c2da8a41
Mara Hart <maralihart@gmail.com>
2021-04-12 09:38:37 -0400

    sent up svg and mobile navbar

--- src/components/Main/Navbar/Navbar.tsx
+++ src/components/Main/Navbar/Navbar.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import Logo from '../../../assets/images/peacock-logo.svg'
 
 function Navbar() {
   return (
-    <nav className="bg-orange d-flex flex-row pr-2 pt-3 pb-3 pl-3 justify-content-between">
-      {/* TODO: Make this image work, also replace with full-navy image */}
-      <img src="../../assets/images/logo-black-white-horizontal.png" alt="Girls Who Code at the University of Virginia logo"/>
-      <a href="/hack" target="_blank" className="mono text-white interactive">Girls Hoo Hack</a>
+    <nav className="bg-orange d-flex flex-row pr-2 pt-3 pb-3 pl-3 justify-content-between align-items-center">
+      <img src={Logo} width="208px" height="82px" alt="Girls Who Code at the University of Virginia logo"/>
+      <a href="/hack" target="_blank" className="mono text-white interactive pr-5 pr-2">Girls Hoo Hack</a>
     </nav>
   );
 }
