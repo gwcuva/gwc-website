965da7551dfd0e2ba26bd4a2b57ee1df94907fdd
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-13 12:41:15 -0400

    fix position

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import {Row, Col} from 'react-bootstrap';
-// import './discordstyle.css';
-// TODO: Add text and link
-// TODO: Add graphic at the end of the page
 
 function DiscordInvite() {
   return (
       <Row>
         <Col sm={12}>
-          <h4 className="text-turq"><b>Want to join Girls Who Code at UVA?</b></h4>
-          <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord">Join our Discord <br />server ---{">"}</h2></a>
+          <h4 className="text-turq pl-lg-5 ml-lg-5 pl-md-5 ml-md-5 pl-sm-5 ml-sm-5">Want to join Girls Who Code at UVA?</h4>
+          <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord pl-lg-5 ml-lg-5 pl-md-5 ml-md-5 pl-sm-5 ml-sm-5">Join our Discord <br />server ---{">"}</h2></a>
           <img src="get started.svg" alt="Orange wave saying 'Get Started'" />
         </Col>
       </Row>
