ae1d87d0da46061656de6b5f6aab9f05819c5328
Mara Hart <maralihart@gmail.com>
2021-04-13 15:35:38 -0400

    changed color of links

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -4,8 +4,8 @@ function NotFound() {
   return (
     <div>
       <h1 className="text-peacock">Page Not Found</h1>
-      <h2 className="text-orange"><a href="/">Go to Main Page</a></h2>
-      <h2 className="text-orange"><a href="/">Go to Hackathon Page</a></h2>
+      <a href="/"><h2 className="text-orange">Go to Main Page</h2></a>
+      <a href="/hack"><h2 className="text-orange">Go to Hackathon Page</h2></a>
     </div>
   );
 }
