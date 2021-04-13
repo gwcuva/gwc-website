b2e8b68fa629408f8265593587a41ee580a42f82
mayatietz <mayatietz@gmail.com>
2021-04-12 23:51:57 -0400

    changed font of links

--- src/components/Main/NavCircle/NavCircle.tsx
+++ src/components/Main/NavCircle/NavCircle.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// TODO: Add circle background (done)
+// TODO: Add three links 
+// TODO: Add social media links (done)
+// TODO: (if needed in a future PR) depending on scroll position, change the color of circle border
+// TODO: Make absolute on the page so it follows the screen (done)
 
 const StyledLinkOne = styled.a`
   position: absolute;
@@ -8,6 +13,7 @@ const StyledLinkOne = styled.a`
     left: 50%;
     margin-right: -50%;
     transform: translate(-50%, -50%)
+    
 
 `
 const StyledLinkTwo = styled.a`
@@ -40,11 +46,6 @@ position: absolute;
     margin-right: -50%;
     transform: translate(-50%, -50%)
 `
-// TODO: Add circle background (done)
-// TODO: Add three links 
-// TODO: Add social media links (done)
-// TODO: (if needed in a future PR) depending on scroll position, change the color of circle border
-// TODO: Make absolute on the page so it follows the screen (done)
 
 const Insta = () => {
   return(
@@ -57,7 +58,7 @@ const Insta = () => {
     <a href="https://www.instagram.com/gwcuva/?hl=en" target="_blank"><img src="#" alt="Instagram" />
       <path
         d="M16 6C10.477 6 6 10.477 6 16v16c0 5.523 4.477 10 10 10h16c5.523 0 10-4.477 10-10V16c0-5.523-4.477-10-10-10zm20 4a1.999 1.999 0 110 4 1.999 1.999 0 110-4zm-12 4c5.523 0 10 4.477 10 10s-4.477 10-10 10-10-4.477-10-10 4.477-10 10-10zm0 4c-3.313 0-6 2.688-6 6 0 3.313 2.688 6 6 6 3.313 0 6-2.688 6-6 0-3.313-2.688-6-6-6zm0 0"
-        fill="#0c3b4f"
+        fill="#003046"
       />
       </a>
     </svg>
@@ -113,7 +114,7 @@ const Discord = () => {
       </defs>
       <path
         d="M42 45.477a.5.5 0 01-.809.394L33 39l.781 2.344a.5.5 0 01-.476.656H10a5 5 0 01-5-5V11a5 5 0 015-5h27a5 5 0 015 5zm0 0"
-        fill="#0c3b4f"
+        fill="#003046"
       />
       <path
         d="M32.629 17.297a.358.358 0 00-.09-.098c-.383-.281-2.805-1.988-5.629-2.199l-.27.55c2.782.669 4.051 1.641 5.38 2.829C29.73 17.21 27.46 16 23.5 16s-6.23 1.21-8.52 2.379c1.329-1.188 2.848-2.27 5.38-2.828L20.09 15c-2.957.285-5.262 1.926-5.63 2.2a.324.324 0 00-.085.093c-.336.512-2.867 4.61-3.363 12.145a.426.426 0 00.11.316c2.554 2.805 6.16 3.187 7.058 3.238a.386.386 0 00.336-.16l.797-1.062c-1.57-.54-3.364-1.508-4.903-3.27 1.84 1.379 4.61 2.5 9.09 2.5 4.48 0 7.25-1.121 9.09-2.5-1.54 1.762-3.328 2.73-4.899 3.27l.793 1.062a.395.395 0 00.336.16c.903-.05 4.504-.433 7.063-3.238a.44.44 0 00.105-.317c-.496-7.53-3.027-11.628-3.36-12.14zM20 28c-1.102 0-2-1.121-2-2.5s.898-2.5 2-2.5c1.102 0 2 1.121 2 2.5s-.898 2.5-2 2.5zm7 0c-1.102 0-2-1.121-2-2.5s.898-2.5 2-2.5c1.102 0 2 1.121 2 2.5s-.898 2.5-2 2.5zm0 0"
@@ -149,17 +150,13 @@ const Circle = () => {
   )
 }
 
-
-      
-
-
 function NavCircle() {
   return (
     <div id = "navCircle" className = "card sticky-top float-right text-center " >
       <Circle />
-      <StyledLinkOne href="#" target="_blank">the gap</StyledLinkOne>
-      <StyledLinkTwo href="#" target="_blank">the mission</StyledLinkTwo>
-      <StyledLinkThree href="#" target="_blank">the team</StyledLinkThree>
+      <StyledLinkOne href="#" target="_blank" className="mono">the gap</StyledLinkOne>
+      <StyledLinkTwo href="#" target="_blank" className="mono">the mission</StyledLinkTwo>
+      <StyledLinkThree href="#" target="_blank" className="mono ">the team</StyledLinkThree>
       <StyledLinkFour>
         <Insta />
       </StyledLinkFour>
