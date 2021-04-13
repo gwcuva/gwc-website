32e7ac4f0616ff642b48f1b5acb6577847156c47
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-13 13:11:30 -0400

    fix image import format

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Row, Col} from 'react-bootstrap';
 import { ArrowRight } from 'react-feather';
+import GetStarted from '../../../assets/images/get started.svg';
 
 function DiscordInvite() {
   return (
@@ -8,7 +9,7 @@ function DiscordInvite() {
         <Col sm={12}>
           <h4 className="text-turq pl-5 ml-5">Want to join Girls Who Code at UVA?</h4>
           <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord pl-5 ml-5">Join our Discord <br />server <ArrowRight color="#003046" size={50} /></h2></a>
-          <img src="get started.svg" alt="Orange wave saying 'Get Started'" />
+          <img src={GetStarted} alt="Orange wave saying 'Get Started'" />
         </Col>
       </Row>
   );
