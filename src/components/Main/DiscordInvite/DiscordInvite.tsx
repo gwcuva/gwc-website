2588041b6ae6d3a2e2024fdf6fb3bac9f2f3fc48
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-13 12:56:35 -0400

    fix padding and margin

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -5,8 +5,8 @@ function DiscordInvite() {
   return (
       <Row>
         <Col sm={12}>
-          <h4 className="text-turq pl-lg-5 ml-lg-5 pl-md-5 ml-md-5 pl-sm-5 ml-sm-5">Want to join Girls Who Code at UVA?</h4>
-          <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord pl-lg-5 ml-lg-5 pl-md-5 ml-md-5 pl-sm-5 ml-sm-5">Join our Discord <br />server ---{">"}</h2></a>
+          <h4 className="text-turq pl-5 ml-5">Want to join Girls Who Code at UVA?</h4>
+          <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord pl-5 ml-5">Join our Discord <br />server ---{">"}</h2></a>
           <img src="get started.svg" alt="Orange wave saying 'Get Started'" />
         </Col>
       </Row>
