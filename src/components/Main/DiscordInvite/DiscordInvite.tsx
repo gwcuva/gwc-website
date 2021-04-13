ca59bb51ca73991c07c3105a33d37c4012ec3dde
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-13 14:09:40 -0400

    make arrow bigger

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -15,7 +15,7 @@ function DiscordInvite(props: Props) {
       <Row>
         <Col>
           <h4 className="text-turq pl-5 ml-5">Want to join Girls Who Code at UVA?</h4>
-          <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord pl-5 ml-5">Join our Discord <br />server <ArrowRight color="#003046" size={50} /></h2></a>
+          <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord pl-5 ml-5">Join our Discord <br />server <ArrowRight color="#003046" size={70} /></h2></a>
           <img src={GetStarted} alt="Orange wave saying 'Get Started'" />
         </Col>
       </Row>
