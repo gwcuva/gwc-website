a267495b3dd2154d711459bda4accbcf7fd5ee8e
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-13 11:42:04 -0400

    replace col with react bootstrap Col

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -7,11 +7,11 @@ import {Row, Col} from 'react-bootstrap';
 function DiscordInvite() {
   return (
     <Row>
-      <div className="col-lg-12 col-md-12 col-sm-12">
+      <Col sm={12}>
         <h4 className="text-turq join"><b>Want to join Girls Who Code at UVA?</b></h4>
         <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord"><b>Join our Discord <br />server ---{">"}</b></h2></a>
         <img src="get started.svg" alt="Orange wave saying 'Get Started'" />
-      </div>
+      </Col>
     </Row>
   );
 }
