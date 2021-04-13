0a8a4793094ad0d8bc260c5c80c25c1c8c5abf46
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-13 14:06:20 -0400

    remove sm 12

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -13,7 +13,7 @@ function DiscordInvite(props: Props) {
   return (
     <div id={props.id}>
       <Row>
-        <Col sm={12}>
+        <Col>
           <h4 className="text-turq pl-5 ml-5">Want to join Girls Who Code at UVA?</h4>
           <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord pl-5 ml-5">Join our Discord <br />server <ArrowRight color="#003046" size={50} /></h2></a>
           <img src={GetStarted} alt="Orange wave saying 'Get Started'" />
