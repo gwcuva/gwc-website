e5da20aef5f78a6675665215783f44ac2f0855ca
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-13 12:29:24 -0400

    replace with react bootstrap

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import './discordstyle.css';
+import {Row, Col} from 'react-bootstrap';
+// import './discordstyle.css';
 // TODO: Add text and link
 // TODO: Add graphic at the end of the page
 
 function DiscordInvite() {
   return (
-    <div>
-      <div className="row">
-
-        <div className="col-lg-12 col-md-12 col-sm-12">
-          <h4 className="text-turq join"><b>Want to join Girls Who Code at UVA?</b></h4>
-          <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord"><b>Join our Discord <br />server ---{">"}</b></h2></a>
+      <Row>
+        <Col sm={12}>
+          <h4 className="text-turq"><b>Want to join Girls Who Code at UVA?</b></h4>
+          <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord">Join our Discord <br />server ---{">"}</h2></a>
           <img src="get started.svg" alt="Orange wave saying 'Get Started'" />
-        </div>
-      </div>
-    </div>
+        </Col>
+      </Row>
   );
 }
 
