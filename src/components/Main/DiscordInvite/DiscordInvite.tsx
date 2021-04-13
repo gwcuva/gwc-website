5553001c77ade41b733d999a43e66b27320f5a6d
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-13 19:34:00 -0400

    add new arrow

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Row, Col} from 'react-bootstrap';
-import { ArrowRight } from 'react-feather';
 import GetStarted from '../../../assets/images/get started.svg';
+import Arrow from '../../../assets/images/arrow-right.svg';
 
 // TODO: Add text and link
 // TODO: Add graphic at the end of the page
@@ -15,7 +15,8 @@ function DiscordInvite(props: Props) {
       <Row>
         <Col>
           <h4 className="text-turq pl-5 ml-5">Want to join Girls Who Code at UVA?</h4>
-          <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord pl-5 ml-5">Join our Discord <br />server <ArrowRight color="#003046" size={70} /></h2></a>
+          <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord pl-5 ml-5">
+            Join our Discord <br />server <img src={Arrow} alt="Orange wave saying 'Get Started'" /></h2></a>
           <img src={GetStarted} alt="Orange wave saying 'Get Started'" />
         </Col>
       </Row>
