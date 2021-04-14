87b2ad65aff18a059268e2fe18264b4110e09f02
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-13 21:32:41 -0400

    change h tags

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -14,9 +14,9 @@ function DiscordInvite(props: Props) {
     <div id={props.id}>
       <Row>
         <Col>
-          <h4 className="text-turq pl-5 ml-5">Want to join Girls Who Code at UVA?</h4>
-          <a href="https://discord.gg/D8knkzNM8E" target="_blank" rel="noopener noreferrer"><h2 className="text-peacock discord pl-5 ml-5">
-            Join our Discord <br />server <img src={Arrow} alt="Orange wave saying 'Get Started'" /></h2></a>
+          <h3 className="text-turq pl-5 ml-5">Want to join Girls Who Code at UVA?</h3>
+          <a href="https://discord.gg/D8knkzNM8E" target="_blank" rel="noopener noreferrer"><h4 className="text-peacock discord pl-5 ml-5">
+            Join our Discord <br />server <img src={Arrow} alt="Orange wave saying 'Get Started'" /></h4></a>
           <img src={GetStarted} alt="Orange wave saying 'Get Started'" />
         </Col>
       </Row>
