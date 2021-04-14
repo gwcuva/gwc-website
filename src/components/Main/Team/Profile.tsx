98b1e554edcff923244807caae38c56802f9fd83
nehagb99 <neha.gb99@gmail.com>
2021-04-13 20:53:58 -0400

    Update Profile.tsx

--- src/components/Main/Team/Profile.tsx
+++ src/components/Main/Team/Profile.tsx
@@ -9,9 +9,9 @@ interface Props {
 function Profile(props: Props) {
   return (
     <div>
-      <img src="#" alt={`Headshot of ${props.name}`} />
-      <p>{props.name}</p>
-      <p className="p2">{props.position}</p>
+      {props.img==="" ? <p></p> : <img className="teamImg d-block mx-auto" src={props.img} alt={`Headshot of ${props.name}`} />}
+      <p className="text-peacock pt-2 text-center">{props.name}</p>
+      <p className="p2 text-center">{props.position==="PR" ? "Public Relations" : props.position}</p>
     </div>
   );
 }
