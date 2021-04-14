e08c3cc1c13cdad9e0b8639de202951c387c124c
pragnya17 <pp8sdv@virginia.edu>
2021-04-14 01:27:03 -0400

    Header for large screen

--- src/components/Main/Header/Header.tsx
+++ src/components/Main/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import {Row, Col} from 'react-bootstrap';
+import Waves from '../../../assets/images/main-header-wave.svg'
 // TODO: Add orange code background (in branding tool-kit graphics)
 // TODO: Add white waves graphic (might need PR help in adding a white background to the wave) -- assets/main-header-wave.svg
 
@@ -9,11 +10,40 @@ interface Props {
 
 function Header(props: Props) {
   return (
-    <div id={props.id}>
-      <h2>HELLO WORLD</h2>
-      <h1>WE'RE MAKING WAVES</h1>
-      <h2>GWC at UVA</h2>
-      <img src="../../assets/images/main-header-wave.svg" alt="Orange wave" />
+    <div id={props.id}> 
+
+    <div className="container-fluid bg-orange">
+      <Row lg={2}>
+      <Col lg={1}><h4></h4></Col>
+      </Row>
+      <Row lg={3}>
+        <Col lg={1}></Col>
+        <Col lg={10}><h2 className="text-light-grey">HELLO WORLD</h2></Col>
+      </Row>
+
+      <Row lg={3}>
+        <Col lg={2}></Col>
+        <Col lg={5}><h1 className="text-white">WE'RE</h1></Col>
+      </Row>
+
+      <Row lg={3}>
+        <Col lg={3}></Col>
+        <h1 className="text-white">MAKING</h1>
+      </Row>
+
+      <Row lg={3}>
+        <Col lg={2}></Col>
+        <h1 className="text-white">WAVES</h1>
+      </Row>
+
+      <Row lg={3}>
+        <Col lg={4}></Col>
+        <Col lg={4}><h2 className="text-light-grey">GWC AT UVA</h2></Col>
+      </Row>
+    </div>
+    
+    <img src={Waves} alt="Orange wave" />
+    
     </div>
   );
 }
