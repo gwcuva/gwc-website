84d816fdc8f97a50b17abfb0acae1ad86fceb1c5
Jennifer <jt4nb@virginia.edu>
2021-04-14 14:12:13 -0400

    fix for inspect

--- src/components/Main/Footer/Contact.tsx
+++ src/components/Main/Footer/Contact.tsx
@@ -11,10 +11,10 @@ function Contact() {
       <h3 style={{paddingBottom: 10}}><strong>Contact</strong></h3>
       <a href="mailto:gwcuva@gmail.com" className="mono text-white interactive">gwcuva@gmail.com</a>
       <Col md={10} style={{marginLeft:-15, marginTop:25}}>
-          <a href="https://www.instagram.com/gwcuva/" target="_blank"><img src={instagramSocial} alt="Instagram icon" style={{paddingRight:15, paddingBottom:15}}/></a>
-          <a href="https://discord.com/invite/rKbWyuDNyG" target="_blank"><img src={discordSocial} alt="Discord icon" style={{paddingRight:15, paddingBottom:15}}/></a>
-          <a href="https://www.facebook.com/gwcuva/" target="_blank"><img src={facebookSocial} alt="Facebook icon" style={{paddingRight:15, paddingBottom:15}}/></a>
-          <a href="https://twitter.com/gwcuva?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank"><img src={twitterSocial} alt="Twitter icon" style={{paddingRight:15, paddingTop:5, paddingBottom:20}}/></a>
+          <a href="https://www.instagram.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={instagramSocial} alt="Instagram icon" style={{paddingRight:15, paddingBottom:15}}/></a>
+          <a href="https://discord.com/invite/rKbWyuDNyG" target="_blank" rel="noreferrer noopener"><img src={discordSocial} alt="Discord icon" style={{paddingRight:15, paddingBottom:15}}/></a>
+          <a href="https://www.facebook.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={facebookSocial} alt="Facebook icon" style={{paddingRight:15, paddingBottom:15}}/></a>
+          <a href="https://twitter.com/gwcuva?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" rel="noreferrer noopener"><img src={twitterSocial} alt="Twitter icon" style={{paddingRight:15, paddingTop:5, paddingBottom:20}}/></a>
       </Col>
     </div>
   );
--- src/components/Main/Footer/Links.tsx
+++ src/components/Main/Footer/Links.tsx
@@ -4,8 +4,8 @@ function Links() {
   return (
     <div>
       <h3 style={{paddingBottom: 10}}><strong>Links</strong></h3>
-      <p><a href="/hack" target="_blank" className="mono text-white">Girls Hoo Hack</a></p>
-      <p><a href="https://www.figma.com/file/eb9ExHXT0yGgo4RkK8FG65/Brand-Manual-Identity-Iterations?node-id=251%3A92" target="_blank" className="mono text-white interactive">Brand Manual</a></p>
+      <p><a href="/hack" target="_blank" rel="noreferrer noopener" className="mono text-white">Girls Hoo Hack</a></p>
+      <p><a href="https://www.figma.com/file/eb9ExHXT0yGgo4RkK8FG65/Brand-Manual-Identity-Iterations?node-id=251%3A92" target="_blank" rel="noreferrer noopener" className="mono text-white interactive">Brand Manual</a></p>
     </div>
   );
 }
