e3762b9698cae3329886f8c213f05c7e6eb26d5e
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-13 21:19:55 -0400

    added noreferrer

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -15,7 +15,7 @@ function DiscordInvite(props: Props) {
       <Row>
         <Col>
           <h4 className="text-turq pl-5 ml-5">Want to join Girls Who Code at UVA?</h4>
-          <a href="https://discord.gg/D8knkzNM8E" target="_blank"><h2 className="text-peacock discord pl-5 ml-5">
+          <a href="https://discord.gg/D8knkzNM8E" target="_blank" rel="noreferrer"><h2 className="text-peacock discord pl-5 ml-5">
             Join our Discord <br />server <img src={Arrow} alt="Orange wave saying 'Get Started'" /></h2></a>
           <img src={GetStarted} alt="Orange wave saying 'Get Started'" />
         </Col>
