33947642fed33f9f7140a4231988ffa15c4e1569
nehagb99 <neha.gb99@gmail.com>
2021-04-14 12:33:00 -0400

    Update Profile.tsx

--- src/components/Main/Team/Profile.tsx
+++ src/components/Main/Team/Profile.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { isMobile } from 'react-device-detect';
 
 interface Props {
   name: string;
@@ -9,9 +10,9 @@ interface Props {
 function Profile(props: Props) {
   return (
     <div>
-      {props.img==="" ? <p></p> : <img className="teamImg d-block mx-auto" src={props.img} alt={`Headshot of ${props.name}`} />}
+      {props.img==="" ? <p></p> : <img className={isMobile ? "teamImgMob d-block mx-auto" : "teamImg d-block mx-auto"} src={props.img} alt={`Headshot of ${props.name}`} />}
       <p className="text-peacock pt-2 text-center">{props.name}</p>
-      <p className="p2 text-center">{props.position==="PR" ? "Public Relations" : props.position}</p>
+      <p className="p2 text-center pt-1">{props.position==="PR" ? "Public Relations" : props.position}</p>
     </div>
   );
 }
