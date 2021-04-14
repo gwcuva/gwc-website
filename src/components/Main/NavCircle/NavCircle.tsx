79b0ff396c777580d2813cb74e90c46de51e1a31
mayatietz <mayatietz@gmail.com>
2021-04-14 15:26:17 -0400

    added circle svg and imported it
    added col/rows; centered text within circle

--- src/components/Main/NavCircle/NavCircle.tsx
+++ src/components/Main/NavCircle/NavCircle.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import {Row, Col} from 'react-bootstrap'
 import styled from 'styled-components';
 import instaLogo from '../../../assets/images/instagram-logo.svg'
 import discordLogo from '../../../assets/images/discord-logo.svg'
+import circle from '../../../assets/images/circle.svg'
 
 // TODO: Add three links 
 // TODO: (if needed in a future PR) depending on scroll position, change the color of circle border
@@ -16,6 +18,7 @@ const Circle = () => {
       overflow="visible"
       preserveAspectRatio="xMinYMin meet"
       
+      
     >
       <circle
         cx={151.5}
@@ -25,25 +28,37 @@ const Circle = () => {
         stroke="#43d6b9"
         strokeWidth={3}
         strokeMiterlimit={10}
+        
       />
     </svg>
+    
 
   )
 }
 
+
 function NavCircle() {
   return (
-    <div id = "navCircle" className = "img sticky-top float-right " >
-      
-      <Circle/>
-    
-      <a href="../Stats/Stats.tsx" target="_blank"  className="mono row text-peacock">the gap</a><br/>
-      <a href="../About/About.tsx" target="_blank"  className="mono row text-peacock">the mission</a><br/>
-      <a href="../Team/Team.tsx" target="_blank"  className="mono row text-peacock">the team</a><br/>
-      <a href="https://www.instagram.com/gwcuva/?hl=en" target="_blank" rel="noreferrer"><img src={instaLogo} alt="Instagram"/>
+    <div id = "navCircle" className = "img sticky-top float-right" >
+      <div style={{ backgroundImage:`url(${circle})`, width:'303px', height:'303px', position: 'sticky',}} className = "img sticky-top float-right">
+      <Col className="">
+        <Row  className="pt-20 pb-3 d-flex justify-content-center">
+        <a href="./Stats/Stats.tsx" target="_blank"  className="mono row text-peacock">the gap</a>
+        </Row>
+        <Row className="pb-3 d-flex justify-content-center">
+        <a href="./About/About.tsx" target="_blank"  className="mono row text-peacock">the mission</a><br/>
+        </Row>
+        <Row className="pb-3 d-flex justify-content-center"> 
+        <a href="./Team/Team.tsx" target="_blank"  className="mono row text-peacock">the team</a><br/>
+        </Row>
+        <Row className="pb-3 d-flex justify-content-center">
+        <a href="https://www.instagram.com/gwcuva/?hl=en" target="_blank" rel="noreferrer"><img src={instaLogo} alt="Instagram"/>
       </a>
       <a href="https://discord.gg/PmER5Fvp" target="_blank" rel="noreferrer"><img src={discordLogo} alt="Discord" />
       </a>
+        </Row>
+      </Col>
+      
       </div>
       
     </div>
