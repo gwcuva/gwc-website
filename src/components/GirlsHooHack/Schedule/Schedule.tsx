395aba289679a21b569828fc7d5eec5e233a4e7f
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2021-04-15 14:19:16 -0400

    added collapse and expand

--- src/components/GirlsHooHack/Schedule/Schedule.tsx
+++ src/components/GirlsHooHack/Schedule/Schedule.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Activity from './Activity';
 import {isMobile} from 'react-device-detect';
 import {Row, Col} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faPlus} from '@fortawesome/free-solid-svg-icons'
 import {faMinus} from '@fortawesome/free-solid-svg-icons'
+import {Collapse} from 'react-collapse';
 
 function Schedule() {
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
+
   return (
     <div className="container-fluid bg-light-grey pt-4 pb-4">
       {isMobile ? 
@@ -19,31 +23,41 @@ function Schedule() {
                 <h3 className="text-peach mono font-weight-bold">Saturday, Oct 17</h3>
               </Col>
               <Col className="d-flex justify-content-end text-peach">
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                <button onClick={() => setOpen1(!open1)} 
+                  aria-controls="collapse-day1" aria-expanded={open1}>
+                  <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                </button>
               </Col>
             </Row>
 
-            <Row className="mt-2">
+            <Collapse isOpened={open1}>
+              <Activity time="9am - 10am" name="Check-in" link="Join Discord"></Activity>
+              <Activity time="10am - 11am" name="Opening Ceremony"></Activity>
+              <Activity time="11am" name="Hacking starts" detail="Team formation"></Activity>
+              <Activity time="11am - 12pm" name="Sustainability workshop"></Activity>
+              <Activity time="11am - 12pm" name="How to Work from Home and Virtual Pair Programming Workshop"></Activity>
+              <Activity time="12pm - 1pm" name="Intro to Hacking Workshop"></Activity>
+              <Activity time="1:30pm - 2:30pm" name="Sponsorship Fair" detail="Google Cloud, Wolfram"></Activity>
+            </Collapse>
+            
+            <Row className="mt-4">
               <Col className="d-flex justify-content-start">
                 <h3 className="text-peach mono font-weight-bold">Sunday, Oct 18</h3>
               </Col>
               <Col className="d-flex justify-content-end text-peach">
-                <FontAwesomeIcon icon={faMinus}></FontAwesomeIcon>
+                <button onClick={() => setOpen2(!open2)} 
+                  aria-controls="collapse-day2" aria-expanded={open2} className="text-peach">
+                  <FontAwesomeIcon icon={faMinus}></FontAwesomeIcon>
+                </button>
               </Col>
             </Row>
-            
-            <Activity time="9am - 10am" name="Check-in" link="Join Discord"></Activity>
-            <Activity time="10am - 11am" name="Opening Ceremony"></Activity>
-            <Activity time="11am" name="Hacking starts" detail="Team formation"></Activity>
-            <Activity time="11am - 12pm" name="Sustainability workshop"></Activity>
-            <Activity time="11am - 12pm" name="How to Work from Home and Virtual Pair Programming Workshop"></Activity>
-            <Activity time="12pm - 1pm" name="Intro to Hacking Workshop"></Activity>
-            <Activity time="1:30pm - 2:30pm" name="Sponsorship Fair" detail="Google Cloud, Wolfram"></Activity>
 
-            <h3 className="text-peach mono font-weight-bold mt-5">Sunday, Oct 18</h3>
-            <Activity time="11am - 12pm" name="Sustainability workshop"></Activity>
-            <Activity time="11am - 12pm" name="How to Work from Home and Virtual Pair Programming Workshop"></Activity>
-            <Activity time="1pm" name="Hacking ends"></Activity>
+            <Collapse isOpened={open2}>
+              <Activity time="11am - 12pm" name="Sustainability workshop"></Activity>
+              <Activity time="11am - 12pm" name="How to Work from Home and Virtual Pair Programming Workshop"></Activity>
+              <Activity time="1pm" name="Hacking ends"></Activity>
+            </Collapse>
+            
           </Col>
         </Row> : 
         <Row className="m-5 pt-5 pb-5">
