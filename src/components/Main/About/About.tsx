b36f77ec076914f8cadb8fc798931748e491a325
Mara Hart <maralihart@gmail.com>
2021-04-19 10:25:26 -0400

    added copy and svg

--- src/components/Main/About/About.tsx
+++ src/components/Main/About/About.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutList from './AboutList';
+import HelloWorld from '../../../assets/images/hello-world.svg'
 
 // TODO: Add Hello World asset on the top (also in branding guide on Google Drive)
 // TODO: Add the copy
@@ -14,9 +15,9 @@ function About(props: Props) {
 
   return (
     <div id={props.id}>
-      <img src="#" alt="Hello World banner" />
-      <h2>Girls Who Code at the University of Virginia...</h2>
-      <p>Together, we're breaking down barriers...</p>
+      <img src={HelloWorld} alt="Hello World banner" />
+      <h2>Girls Who Code at the University of Virginia is an empowering and inclusive team of the next leaders in CS.</h2>
+      <p>Together, we're breaking down barriers in the tech industry due to resource inequality, race, and gender biases. All skill levels welcome. Come for the coding, stay for the community!</p>
 
       <AboutList header="Social Events" items={socialEvents}/>
       <AboutList header="Social Events" items={socialEvents}/>
