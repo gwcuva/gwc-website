979948b7ed1d4523f63c1042dce8f982c5cc110d
Jennifer <jt4nb@virginia.edu>
2021-04-21 18:08:33 -0400

    alignment

--- src/components/Main/Footer/Footer.tsx
+++ src/components/Main/Footer/Footer.tsx
@@ -17,9 +17,9 @@ function Footer(props: Props) {
       <img src={footerWaves} style={{transform: 'rotate(180deg)', marginBottom:-1}} alt="Orange wave"/>
       <div className="bg-orange d-flex flex-row pb-5 pt-5">
         <Col md={isMobile ? 0 : 4}>
-          <img src={logo} width={isMobile ? "112px" : "208px"} height={isMobile ? "44px" : "82px"} className={isMobile ? "pl-1" : "pl-5"} alt="Girls Who Code at the University of Virginia logo"/>
+          <img src={logo} width={isMobile ? "112px" : "208px"} height={isMobile ? "44px" : "82px"} className={isMobile ? "pl-2" : "pl-5"} alt="Girls Who Code at the University of Virginia logo"/>
         </Col>
-        <Col md={10} sm={6}>
+        <Col md={10}>
           <Row>
             <Col md={2} sm={10} className="text-white" style={{marginBottom:60}}>
               <Contact />
