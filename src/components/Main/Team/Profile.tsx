a091bedc4de721ecd2ab23552b537fe10d292009
nehagb99 <neha.gb99@gmail.com>
2021-04-21 14:55:24 -0400

    Update Profile.tsx

--- src/components/Main/Team/Profile.tsx
+++ src/components/Main/Team/Profile.tsx
@@ -8,11 +8,38 @@ interface Props {
 }
 
 function Profile(props: Props) {
+  var keys = [
+    "VP",
+    "PR",
+    "Hackathon",
+    "Secretary",
+    "Treasurer",
+    "President",
+    "Curriculum",
+    "Membership",
+  ]
+  var values = [
+    "Vice President",
+    "Public Relations",
+    "Director of Hackathon",
+    "Secretary",
+    "Treasurer",
+    "President",
+    "Director of Curriculum",
+    "Director of Membership",
+  ];
+
+  var positions = new Map();
+
+  for(var i = 0; i < keys.length; i++){
+    positions.set(keys[i], values[i]);
+  }
+
   return (
     <div>
-      {props.img && <img className="teamImg d-block mx-auto" src={props.img} alt={`Headshot of ${props.name}`} />}
+      {props.img && <img width={isMobile ? "120px" : "185px"} height={isMobile ? "120px" : "185px"} className="teamImg d-block mx-auto" src={props.img} alt={`Headshot of ${props.name}`} />}
       <p className="text-peacock pt-2 text-center">{props.name}</p>
-      <p className="p2 text-center">{props.position==="PR" ? "Public Relations" : props.position}</p>
+      <p className="p2 text-center">{positions.get(props.position)}</p>
     </div>
   );
 }
