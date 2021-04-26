46fdc4b7f55b7bbbb53cce2a5cc95caba205a0be
Jennifer <jt4nb@virginia.edu>
2021-04-26 00:44:53 -0400

    footer waves + logo added

--- src/components/GirlsHooHack/Footer/Footer.tsx
+++ src/components/GirlsHooHack/Footer/Footer.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-
+import blueWaves from '../../../assets/images/ghh-footer-waves.svg';
+import whiteLogo from '../../../assets/images/white-logo.svg';
+import {Row, Col} from 'react-bootstrap';
+import {isMobile} from 'react-device-detect';
+ 
 function Footer() {
   return (
-    <p>Footer</p>
+    <div className="bg-peacock">
+      <img src={whiteLogo} width="359px" height="114px" alt="logo"/>
+      <img src={blueWaves} alt="" style={{width: "100%"}}/>
+    </div>
   );
 }
 
