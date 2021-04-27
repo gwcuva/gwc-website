77a4d45e02bcb55f9844d7dbd852d8b816e13458
nehagb99 <neha.gb99@gmail.com>
2021-04-27 19:00:11 -0400

    Update FAQ.tsx

--- src/components/GirlsHooHack/FAQ/FAQ.tsx
+++ src/components/GirlsHooHack/FAQ/FAQ.tsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { request } from 'graphql-request';
+import {Row, Col} from 'react-bootstrap';
+import { isMobile } from 'react-device-detect';
 
 function FAQ() {
+
+  const [faq, setFaq] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchFaq = async () => {
+      const { questionAnswers } = await request(
+        process.env.REACT_APP_GRAPHCMS_URL ? process.env.REACT_APP_GRAPHCMS_URL : "",
+        `
+          { 
+            questionAnswers {
+              question
+              answer
+            }
+          }
+        `
+      );
+      setFaq(questionAnswers);
+    };
+
+    fetchFaq();
+  }, []);
+
   return (
     <p>FAQ</p>
   );
