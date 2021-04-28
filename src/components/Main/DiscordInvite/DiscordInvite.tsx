50fe37ed9679947a16219c7bf5374fbb6703e248
Mara Hart <maralihart@gmail.com>
2021-04-28 09:46:05 -0400

    discord invite formatted

--- src/components/Main/DiscordInvite/DiscordInvite.tsx
+++ src/components/Main/DiscordInvite/DiscordInvite.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import {Row, Col} from 'react-bootstrap';
 import Arrow from '../../../assets/images/arrow-right.svg';
+import {isMobile} from 'react-device-detect';
 
-// TODO: Add text and link
-// TODO: Add graphic at the end of the page
 interface Props {
   id: string;
 }
 
 function DiscordInvite(props: Props) {
   return (
-    <Row id={props.id} className="m-4">
-      <Col lg={6} md={8} sm={10}>
+    <Row id={props.id} className="ml-1 justify-content-center">
+      <Col sm={10} xs={12}>
         <h3 className="text-turq">Want to join Girls Who Code at UVA?</h3>
         <a href="https://discord.gg/D8knkzNM8E" target="_blank" rel="noopener noreferrer">
           <h4 className="text-peacock mono">
-            Join our Discord server <img src={Arrow} alt="Orange wave saying 'Get Started'" />
+            Join our Discord server <img src={Arrow} width={isMobile ? "41px" : "72px"} alt="Orange wave saying 'Get Started'" />
           </h4>
         </a>
       </Col>
