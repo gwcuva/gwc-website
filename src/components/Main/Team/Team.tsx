4ee4c3034a2871e0e381f92640f9a41318028f8c
Mara Hart <maralihart@gmail.com>
2021-04-28 13:10:50 -0400

    set up archive and filtering

--- src/components/Main/Team/Team.tsx
+++ src/components/Main/Team/Team.tsx
@@ -4,14 +4,13 @@ import { request } from 'graphql-request';
 import {Row, Col} from 'react-bootstrap';
 import { isMobile } from 'react-device-detect';
 
-// TODO: (can be separate PR), have an archive of old exec
 interface Props {
   id: string;
 }
 
 function Team(props: Props) {
-  const [team, setTeam] = useState([{'id': '', 'name': '', 'position': '', 'academicYear': '', 'headshot': {'url': ''}}]);
-  const [academicYear, setAcademicYear] = useState('second2122');
+  const [team, setTeam] = useState([{'id': '', 'name': '', 'position': '', 'academicYear': '', 'headshot': {'url': ''}, 'linkedin': ''}]);
+  const [academicYear, setAcademicYear] = useState('');
   const [boardYears, setBoardYears] = useState(['']);
 
   useEffect(() => {
@@ -28,20 +27,21 @@ function Team(props: Props) {
               headshot {
                 url
               }
+              linkedin
             }
           }
         `
       );
       
-      setTeam(executiveMembers);
       let uniqueYears = [];
       for (let i = 0; i < executiveMembers.length; i++) {
         if (uniqueYears.indexOf(executiveMembers[i].academicYear) === -1) {
           uniqueYears.push(executiveMembers[i].academicYear)
         }
       }
-      console.log(uniqueYears)
-      setBoardYears(uniqueYears);
+      setTeam(executiveMembers);
+      setBoardYears([...uniqueYears]);
+      setAcademicYear(uniqueYears.pop());
     };
 
     fetchTeam();
@@ -55,11 +55,15 @@ function Team(props: Props) {
           <p className="text-peacock pt-3">The best work is produced when diverse voices help create it. Our leadership team works together to disrupt the image of the stereotypical programmer. Meet our wave-makers!</p>
 
           {boardYears.map((year, index) => 
-            <button className="interactive text-orange button-unstyled" onClick={() => setAcademicYear(year)}>
+            <button 
+              className={`interactive ${year === academicYear ? 'text-blue' : 'text-orange'} button-unstyled `} 
+              onClick={() => setAcademicYear(year)} 
+              disabled={year === academicYear}
+              >
               {year && 
                 (year === "first2021" ? "Founding Board (2020-2021)" 
                 : ("20" + year.substring(year.length-4, year.length-2) + "-20" + year.substring(year.length-2, year.length)))} 
-              {index !== boardYears.length - 1 && <span className="px-1 text-peacock">|</span>}
+              {(index !== boardYears.length - 1) && <span className="px-1 text-peacock">|</span>}
             </button>)}
         </Col>
       </Row>
@@ -67,7 +71,7 @@ function Team(props: Props) {
         <Col lg={6} md={8} sm={10} xs={11}>
           <Row className="justify-content-around">
             {team.filter((mem) => mem.academicYear === academicYear).map(mem => 
-              <Profile name={mem.name} img={mem.headshot.url} position={mem.position} />
+              <Profile mem={mem} />
             )}
           </Row>
         </Col>
