bffeca59e1ac326869be5ba7ea8c7d522c8406df
jennifertcheou <60328475+jennifertcheou@users.noreply.github.com>
2021-04-28 09:58:40 -0400

    Jt4nb/footer (#80)
    * added basic links and layout
    
    * added social links + svgs and changed column sizes
    
    * images and formatting
    
    * fixed line and overflow
    
    * Update package-lock.json
    
    * more styling (mobile)
    
    * switch to Row and Col tags
    
    * added email link
    
    * removed comments
    
    * fix for inspect
    
    * alt tag
    
    * update
    
    * mobile view
    
    * alignment
    
    Co-authored-by: Mara Hart <maralihart@gmail.com>

--- src/components/Main/Footer/Footer.tsx
+++ src/components/Main/Footer/Footer.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import Contact from './Contact';
 import Links from './Links';
+import footerWaves from '../../../assets/images/footer-wave.svg';
+import logo from '../../../assets/images/peacock-logo.svg';
+import {Row, Col} from 'react-bootstrap';
+import {isMobile} from 'react-device-detect';
 
-// TODO: Add top graphic (should be same as header, but flipped)
-// TODO: Add logo
-// TODO: Add contact
-// TODO: Add links (Brand Manual should redirect to Figma since we haven't coded it)
 interface Props {
   id: string;
 }
 
+
 function Footer(props: Props) {
   return (
     <div id={props.id}>
-      <img src="#" alt="Girls Who Code at the University of Virginia logo" />
-      <Contact />
-      <Links />
+      <img src={footerWaves} style={{transform: 'rotate(180deg)', marginBottom:-1}} alt="Orange wave"/>
+      <div className="bg-orange d-flex flex-row pb-5 pt-5">
+        <Col md={isMobile ? 0 : 4}>
+          <img src={logo} width={isMobile ? "112px" : "208px"} height={isMobile ? "44px" : "82px"} className={isMobile ? "pl-2" : "pl-5"} alt="Girls Who Code at the University of Virginia logo"/>
+        </Col>
+        <Col md={10}>
+          <Row>
+            <Col md={2} sm={10} className="text-white" style={{marginBottom:60}}>
+              <Contact />
+            </Col>
+            <Col md={3} sm={4} className="text-white" style={{marginBottom:60}}>
+              <Links />
+            </Col>
+          </Row>
+        </Col>
+      </div>
     </div>
   );
 }
