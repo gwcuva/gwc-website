abb35e57679c1c8bf707fa5c3bfcb3ca6390e020
Mara Hart <maralihart@gmail.com>
2021-04-28 11:32:07 -0400

    adjusted spacing according to mobile or desktop view

--- src/components/GirlsHooHack/About/About.tsx
+++ src/components/GirlsHooHack/About/About.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
+import { isMobile } from 'react-device-detect';
 
 function About() {
   return (
-  <Row className="bg-hack-grey pt-5">
-    <Col className="ml-5">
-      <h2 className="text-orange">About</h2>
-      <Row>
-        <Col md={4} className="mb-5 mt-4">
+  <Row className={`bg-hack-grey justify-content-center ${isMobile ? "" : ""} py-5`}>
+    <Col sm={10} xs={11} className={`my-5 ${isMobile ? "" : "py-5"}`}>
+      <h2 className="text-orange mobile-upper">About</h2>
+      <Row className={`${isMobile ? "" : "my-5"}`}>
+        <Col md={4} xs={12} className={`${isMobile ? "my-3" : "mr-5"}`}>
           <h3 className="mono text-peach">Girls Hoo Hack</h3>
           <p>Girls Hoo Hack is a virtual 48-hour hackathon hosted by Girls Who Code at UVA that aims to foster a community of diverse thought and skill.</p> 
           <p>Coders of all levels of experience have the opportunity to work together with teams and mentors, attend educational workshops, and code action-oriented projects that reflect innovation, diverse perspectives, and empowerment.</p>
         </Col>
-        <Col md={4} className="mb-5 mt-4">
+        <Col md={4} xs={12} className={`${isMobile ? "" : ""}`}>
           <h3 className="mono text-peach">GWC @ UVA</h3>
           <p>Girls Who Code at the University of Virginia (UVA) is a College Loop of the nationwide Girls Who Code nonprofit organization. GWC at UVA aims to close the gender gap in technology by creating opportunities for young women to explore technology.</p>
         </Col>
