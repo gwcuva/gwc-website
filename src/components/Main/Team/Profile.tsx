77440661e2f4ed0fac4e53b171be1a89e2f79d15
Mara Hart <42477471+maralihart@users.noreply.github.com>
2021-04-28 13:15:07 -0400

    Mlh4xc/filtering exec (#91)
    * set up filtering
    
    * set up archive and filtering

--- src/components/Main/Team/Profile.tsx
+++ src/components/Main/Team/Profile.tsx
@@ -3,12 +3,18 @@ import { Col, Row } from 'react-bootstrap';
 import { isMobile } from 'react-device-detect';
 
 interface Props {
-  name: string;
-  img: string;
-  position: string;
+  mem: {
+    id: string,
+    name: string,
+    position: string,
+    headshot: { url: string },
+    linkedin: string;
+  }
 }
 
 function Profile(props: Props) {
+
+  const mem = props.mem
 
   const positions = new Map([
     ["President", "President"],
@@ -23,9 +29,11 @@ function Profile(props: Props) {
 
   return (
     <Col xs={6} md={4}>
-      <Row className="justify-content-center">{props.img && <img width={isMobile ? "100px" : "156px"} height={isMobile ? "100px" : "156px"} className="circleImg" src={props.img} alt={`Headshot of ${props.name}`} />}</Row>
-      <p className="text-peacock mt-2 mb-0 text-center">{props.img && props.name}</p>
-      <p className="text-peacock p2 text-center">{props.position && positions.get(props.position)}</p>
+      <a href={`https://linkedin.com/in/${mem.linkedin}`} target="_blank" rel="noopener noreferrer">
+        <Row className="justify-content-center">{mem.headshot.url && <img width={isMobile ? "100px" : "156px"} height={isMobile ? "100px" : "156px"} className="circleImg" src={mem.headshot.url} alt={`Headshot of ${mem.name}`} />}</Row>
+        <p className="text-peacock mt-2 mb-0 text-center">{mem.headshot.url && mem.name}</p>
+        <p className="text-peacock p2 text-center">{mem.position && positions.get(mem.position)}</p>
+      </a>
     </Col>
   );
 }
