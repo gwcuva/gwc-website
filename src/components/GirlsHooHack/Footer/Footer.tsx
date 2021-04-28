96008632e971472098439a3428a7c610261b51f5
Jennifer <jt4nb@virginia.edu>
2021-04-28 01:14:19 -0400

    sponsors

--- src/components/GirlsHooHack/Footer/Footer.tsx
+++ src/components/GirlsHooHack/Footer/Footer.tsx
@@ -13,17 +13,20 @@ function Footer() {
   return (
     <div>
       <div className="bg-peacock" style={{width:"100%"}}>
-        <div className="ml-4 pt-6">
-          <img src={whiteLogo} className="mb-4" width="359px" height="114px" alt="logo" style={{position:'relative', zIndex: 100, top:'370px', left:'100px'}}/>
-          <Row style={{position:'relative', top:'370px', left:'115px'}}>
-            <img src={email} className="mr-4" alt="logo"/>
-            <a href="https://www.instagram.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={instagram} className="mr-4" alt="logo"/></a>
-            <a href="https://discord.com/invite/rKbWyuDNyG" target="_blank" rel="noreferrer noopener"><img src={discord} className="mr-4" alt="logo"/></a>
-            <a href="https://www.facebook.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={facebook} className="mr-4" alt="logo"/></a>
-            <a href="https://twitter.com/gwcuva" target="_blank" rel="noreferrer noopener"><img src={twitter} className="mr-4" alt="logo"/></a>
-          </Row>
+        <div className={isMobile ? "" : "pt-6 bg-white"}>
+          <div className="ml-4">
+            <img src={whiteLogo} className="mb-4" width={isMobile ? "112px" : "359px"} height={isMobile ? "35.57px" : "114px"} alt="logo" style={{position:'relative', zIndex: 100, top:'370px', left:'100px'}}/>
+            <Row style={{position:'relative', top:'370px', left:'115px'}}>
+              <a href="mailto:gwcuva@gmail.com" className="mono text-white interactive"><img src={email} className="mr-4" alt="logo"/></a>
+              <a href="https://www.instagram.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={instagram} className="mr-4" alt="logo"/></a>
+              <a href="https://discord.com/invite/rKbWyuDNyG" target="_blank" rel="noreferrer noopener"><img src={discord} className="mr-4" alt="logo"/></a>
+              <a href="https://www.facebook.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={facebook} className="mr-4" alt="logo"/></a>
+              <a href="https://twitter.com/gwcuva" target="_blank" rel="noreferrer noopener"><img src={twitter} className="mr-4" alt="logo"/></a>
+            </Row>
+          </div>
         </div>
         <img src={blueWaves} alt="" style={{width: "100%", position: 'relative'}}/>
+        
       </div>
     </div>
   );
--- src/components/GirlsHooHack/Sponsors/Sponsors.tsx
+++ src/components/GirlsHooHack/Sponsors/Sponsors.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import googleCloud from '../../../assets/images/google-cloud.svg';
-import stickerMule from '../../../assets/images/sticker-mule.svg';
-import wolfram from '../../../assets/images/wolfram.svg';
-import hackerearth from '../../../assets/images/hackerearth.svg';
-import uvads from '../../../assets/images/uva-datascience.svg';
+import googleCloud from '../../../assets/images/google-cloud.png';
+import stickerMule from '../../../assets/images/sticker-mule.png';
+import wolfram from '../../../assets/images/wolfram.png';
+import hackerearth from '../../../assets/images/hackerearth.png';
+import uvads from '../../../assets/images/uva-datascience.png';
+import {Row, Col} from 'react-bootstrap';
+import {isMobile} from 'react-device-detect';
 
 function Sponsors() {
   return (
-    <div>
-      <img src={googleCloud} alt=""/>
-      <img src={stickerMule} alt=""/>
-      <img src={wolfram} alt=""/>
-      <img src={hackerearth} alt=""/>
-      <img src={uvads} alt=""/>
+    <div className="mx-auto pt-1 mt-1" style={{width:"85%"}}>
+      <h2 className="text-orange py-5">Sponsors</h2>
+      <img src={googleCloud} className={isMobile ? "row mx-auto" : ""} width="132.6px" height="132.6px" style={{marginRight:"8%"}} alt=""/>
+      <img src={stickerMule} className={isMobile ? "row mx-auto mt-3 mb-4" : ""} width="220.61px" height="75px"style={{marginRight:"8%"}} alt=""/>
+      <img src={wolfram} className={isMobile ? "row mx-auto mb-4" : ""} width="115px" height="91px" style={{marginRight:"10%"}} alt=""/>
+      <img src={hackerearth} className={isMobile ? "row mx-auto mt-5 mb-5" : ""} width="149px" height="30px" style={{marginRight:"10%"}} alt=""/>
+      <img src={uvads} className={isMobile ? "row mx-auto my-5" : ""} width="100.21px" height="100.21px" alt=""/>
     </div>
   );
 }
