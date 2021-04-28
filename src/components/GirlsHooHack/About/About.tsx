206b45ae9fcf5a41298c893a350c28ca41f375c0
Mara Hart <maralihart@gmail.com>
2021-04-28 11:09:34 -0400

    edited about

--- src/components/GirlsHooHack/About/About.tsx
+++ src/components/GirlsHooHack/About/About.tsx
@@ -3,16 +3,16 @@ import { Col, Row } from 'react-bootstrap';
 
 function About() {
   return (
-  <Row>
+  <Row className="bg-hack-grey pt-5">
     <Col className="ml-5">
       <h2 className="text-orange">About</h2>
       <Row>
-        <Col className="col-md-3 mb-5 mt-4">
+        <Col md={4} className="mb-5 mt-4">
           <h3 className="mono text-peach">Girls Hoo Hack</h3>
           <p>Girls Hoo Hack is a virtual 48-hour hackathon hosted by Girls Who Code at UVA that aims to foster a community of diverse thought and skill.</p> 
           <p>Coders of all levels of experience have the opportunity to work together with teams and mentors, attend educational workshops, and code action-oriented projects that reflect innovation, diverse perspectives, and empowerment.</p>
         </Col>
-        <Col className="col-md-3 mb-5 mt-4">
+        <Col md={4} className="mb-5 mt-4">
           <h3 className="mono text-peach">GWC @ UVA</h3>
           <p>Girls Who Code at the University of Virginia (UVA) is a College Loop of the nationwide Girls Who Code nonprofit organization. GWC at UVA aims to close the gender gap in technology by creating opportunities for young women to explore technology.</p>
         </Col>
