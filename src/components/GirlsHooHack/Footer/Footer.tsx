90a09b28b56e4d86b5d7135033ba73bd4e7e5e3a
Jennifer <jt4nb@virginia.edu>
2021-04-27 23:24:20 -0400

    footer links and fixed alignment

--- src/components/GirlsHooHack/Footer/Footer.tsx
+++ src/components/GirlsHooHack/Footer/Footer.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import blueWaves from '../../../assets/images/ghh-footer-waves.svg';
-import whiteLogo from '../../../assets/images/white-logo.svg';
+import blueWaves from '../../../assets/images/footerWaveGHH.png';
+import whiteLogo from '../../../assets/images/whitelogo.png';
+import email from '../../../assets/images/envelope-solid.svg';
+import instagram from '../../../assets/images/instagram-square-brands.svg';
+import discord from '../../../assets/images/discord-brands.svg';
+import facebook from '../../../assets/images/facebook-f-brands.svg';
+import twitter from '../../../assets/images/twitter-brands.svg';
 import {Row, Col} from 'react-bootstrap';
 import {isMobile} from 'react-device-detect';
  
 function Footer() {
   return (
-    <div className="bg-peacock">
-      <img src={whiteLogo} width="359px" height="114px" alt="logo"/>
-      <img src={blueWaves} alt="" style={{width: "100%"}}/>
+    <div>
+      <div className="bg-peacock" style={{width:"100%"}}>
+        <div className="ml-4 pt-6">
+          <img src={whiteLogo} className="mb-4" width="359px" height="114px" alt="logo" style={{position:'relative', zIndex: 100, top:'370px', left:'100px'}}/>
+          <Row style={{position:'relative', top:'370px', left:'115px'}}>
+            <img src={email} className="mr-4" alt="logo"/>
+            <a href="https://www.instagram.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={instagram} className="mr-4" alt="logo"/></a>
+            <a href="https://discord.com/invite/rKbWyuDNyG" target="_blank" rel="noreferrer noopener"><img src={discord} className="mr-4" alt="logo"/></a>
+            <a href="https://www.facebook.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={facebook} className="mr-4" alt="logo"/></a>
+            <a href="https://twitter.com/gwcuva" target="_blank" rel="noreferrer noopener"><img src={twitter} className="mr-4" alt="logo"/></a>
+          </Row>
+        </div>
+        <img src={blueWaves} alt="" style={{width: "100%", position: 'relative'}}/>
+      </div>
     </div>
   );
 }
--- src/components/GirlsHooHack/Sponsors/Sponsors.tsx
+++ src/components/GirlsHooHack/Sponsors/Sponsors.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import googleCloud from '../../../assets/images/google-cloud.svg';
+import stickerMule from '../../../assets/images/sticker-mule.svg';
+import wolfram from '../../../assets/images/wolfram.svg';
+import hackerearth from '../../../assets/images/hackerearth.svg';
+import uvads from '../../../assets/images/uva-datascience.svg';
 
 function Sponsors() {
   return (
-    <p>Sponsors</p>
+    <div>
+      <img src={googleCloud} alt=""/>
+      <img src={stickerMule} alt=""/>
+      <img src={wolfram} alt=""/>
+      <img src={hackerearth} alt=""/>
+      <img src={uvads} alt=""/>
+    </div>
   );
 }
 
