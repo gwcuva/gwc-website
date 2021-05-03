acfe710c96c715408262b6c3188c4628eecc619c
pragnya17 <pp8sdv@virginia.edu>
2021-05-03 00:52:14 -0400

    Desktop version of header

--- src/components/GirlsHooHack/Header/Header.tsx
+++ src/components/GirlsHooHack/Header/Header.tsx
@@ -3,10 +3,14 @@ import {Row, Col} from 'react-bootstrap';
 import {isMobile} from 'react-device-detect';
 import logo from '../../../assets/images/peacock-logo.svg';
 import Waves from '../../../assets/images/hack-header-waves.svg';
-import SignUp from '../../../assets/images/sign-up-hack.svg';
+import SignUpCircle from '../../../assets/images/sign-up-hack-circle.svg';
 import UVA from '../../../assets/images/uva.svg';
+import SignUpText from '../../../assets/images/sign-up-text.svg';
+import SignUpBanner from '../../../assets/images/sign-up-banner.svg';
+import MLHBanner from '../../../assets/images/mlh-banner.svg';
 
 interface Props {
+  toggle? : () => void;
   id: string;
 }
 
@@ -18,33 +22,31 @@ function Header(props: Props) {
               backgroundPosition: "0px 375px",
               width:"100%" }}>
 
-        <Col xs={isMobile ? 0 : 4} className="pt-2">
-          <img src={logo} width={isMobile ? "112px" : "208px"} height={isMobile ? "44px" : "82px"} className={isMobile ? "pl-2" : "pl-5"} alt="Girls Who Code at the University of Virginia logo"/>
-        </Col>
-
-        <Row xs={12} className={isMobile ? "pt-3":"pt-5"}></Row>
-        <Row xs={12} className={isMobile ? "pt-3":"pt-5"}></Row>
-
-        <Row xs={12} className={isMobile ? "pt-3":"pt-5"}>
-          <Row>
-            <Col xs={1}></Col>
-            <Col xs={3}><h1 className={isMobile ? "text-white ml-n3":"text-white pl-5 ml-5"}>GIRLS</h1></Col>         
-          </Row>
-
-          <Row xs={12} className="pl-5 pr-5 text-right">
-            <Row>
-              <Col xs={12} className="pl-5">
-                <img src={UVA}/>
-                <h3>November</h3>
-                <h3>6-7</h3>
-              </Col>
-            </Row>
-            
-          </Row>
-
+        <Row>
+          <Col xs={9} className="pt-2 pr-5">
+            <img src={logo} width="208px" height="82px" className="pl-5" alt="Girls Who Code at the University of Virginia logo"/>
+          </Col>
+          <Col xs={1} className="pl-5">
+            <img src={SignUpBanner} alt="Sign up banner"/>
+          </Col>
+          <Col xs={1} className="pl-5">
+            <img src={MLHBanner} alt="MLH banner"/>
+          </Col>
         </Row>
 
-        
+        <Row xs={12} className="pt-5"></Row>
+        <Row xs={12} className="pt-5"></Row>
+
+        <Row xs={12} className="pl-5">
+            <Col xs={1}></Col>
+            <Col md={8}><h1 className="text-white pl-5">GIRLS</h1></Col>         
+
+            <Col xs={3} className="text-blue">
+              <img src={UVA}/>
+              <h3>November</h3>
+              <h3>06-07</h3>
+            </Col>
+        </Row>
 
         <Row xs={12} className="mt-n5">
           <Col xs={1}></Col>
@@ -52,15 +54,31 @@ function Header(props: Props) {
           <Col xs={4}><h2 className={isMobile ? "text-blue":"text-blue font-weight-bold pt-4"}>2020 VIRTUAL HACKATHON</h2></Col>
         </Row>
 
-        <Row className={isMobile ? "pt-0 mt-n4":"pt-0 mt-n5"}>
+        <Row className="pt-0 mt-n5 pb-5">
           <Col xs={2}></Col>
-          <h1 className="text-white">HACK</h1>
+          <Col xs={6}><h1 className="text-white">HACK</h1></Col>
+          <Col 
+          style={{backgroundImage: `url(${SignUpText})`, 
+          backgroundRepeat:"no-repeat",
+          backgroundPosition: "95px 100px"}}>
+            <a href="https://discord.gg/D8knkzNM8E" onClick={props.toggle}>
+              <img src={SignUpCircle} alt="Sign Up Circle"/>
+            </a>
+          </Col>
         </Row>
-        
+
         <Row xs={12} className="pt-5 pb-5"></Row>
         <Row xs={12} className="pt-5 pb-5"></Row>
         <Row xs={12} className="pt-5 pb-5"></Row>
-        <Row xs={12} className="pt-5 pb-5"></Row>
+
+        <Row xs={12}>
+          <Col xs={2}></Col>
+          <Col xs={2}><h5>About</h5></Col>
+          <Col xs={2}><h5>JumpStart</h5></Col>
+          <Col xs={2}><h5>Schedule</h5></Col>
+          <Col xs={2}><h5>Sponsors</h5></Col>
+          <Col xs={2}><h5>FAQ</h5></Col>
+        </Row>
 
     </div>
   );
