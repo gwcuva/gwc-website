0dbc09df3ae120571b53f850d7e2625747e36cd7
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2021-05-09 20:56:30 -0400

    started querying graphcms

--- src/components/GirlsHooHack/Schedule/Schedule.tsx
+++ src/components/GirlsHooHack/Schedule/Schedule.tsx
@@ -1,15 +1,57 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState} from 'react';
 import Activity from './Activity';
-import {isMobile} from 'react-device-detect';
-import {Row, Col} from 'react-bootstrap';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faPlus} from '@fortawesome/free-solid-svg-icons'
-import {faMinus} from '@fortawesome/free-solid-svg-icons'
-import {Collapse} from 'react-collapse';
+import { request } from 'graphql-request';
+import { isMobile } from 'react-device-detect';
+import { Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faMinus } from '@fortawesome/free-solid-svg-icons'
+import {Collapse } from 'react-collapse';
 
 function Schedule() {
+  const [events, setEvents] = useState([{'id': '', 'eventName': '', 'link': '', 'time': '', 'description': ''}]);
+  const [dates, setDates] = useState(['']);
+
   const [open1, setOpen1] = useState(false);
   const [open2, setOpen2] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const { hackathonEvents } = await request(
+        process.env.REACT_APP_GRAPHCMS_URL ? process.env.REACT_APP_GRAPHCMS_URL : "",
+        `
+          { 
+            hackathonEvents {
+              id
+              eventName
+              link
+              time
+              description
+            }
+          }
+        `
+      );
+      
+      
+      // let uniqueDates = [];
+      for (let i = 0; i < hackathonEvents.length; i++) {
+        console.log(hackathonEvents[i].eventName);
+        // if (uniqueDates.indexOf(hackathonEvents[i].time) === -1) {
+        //   uniqueDates.push(hackathonEvents[i].time)
+        // }
+      }
+      setEvents(hackathonEvents);
+      // setDates([...uniqueDates]);
+    };
+
+    fetchEvents();
+  }, []);
+
+  // console.log(events);
+  console.log("num events: " + events.length);
+  for (let i = 0; i < events.length; i++) {
+    console.log(events[i].eventName);
+  }
 
   return (
     <div className="container-fluid bg-light-grey pt-4 pb-4">
@@ -65,6 +107,17 @@ function Schedule() {
         <Row className="m-5 pt-5 pb-5">
           <Col md={7} sm={10} xs={12} className="ml-5">
             <h2 className="text-orange font-weight-bold">Schedule</h2>
+            <h2>{events[0].eventName}</h2>
+            <h2>{events[0].time}</h2>
+            <h3>{events[0].description}</h3>
+
+            <h2>{events[1].eventName}</h2>
+            <h2>{events[1].time}</h2>
+            <h3>{events[1].description}</h3>
+
+            <h2>{events[2].eventName}</h2>
+            <h2>{events[2].time}</h2>
+            <h3>{events[2].description}</h3>
 
             <h3 className="text-peach mono font-weight-bold mt-4">Saturday, Oct 17</h3>
             <Activity time="9am - 10am" name="Check-in" link="Join Discord"></Activity>
