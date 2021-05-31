a0e21d1d5a59e61a88ea42349b5f4d41a6ba8634
nehagb99 <neha.gb99@gmail.com>
2021-05-30 21:24:59 -0400

    Update FAQComponent.tsx

--- src/components/GirlsHooHack/FAQ/FAQComponent.tsx
+++ src/components/GirlsHooHack/FAQ/FAQComponent.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { isMobile } from 'react-device-detect';
+import { Collapse } from 'react-collapse';
+import { FaFontAwesome } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+import { FaMinus } from "react-icons/fa";
+import {Row, Col} from 'react-bootstrap';
 
 interface Props {
   question: string;
@@ -7,10 +12,25 @@ interface Props {
 }
 
 function FAQComponent(props: Props) {
+
+  const [open, setOpen] = useState(false);
+  const [hovColor, setHovColor] = useState(false);
+
   return (
-    <div className="mt-4">
-      <p className="mono font-weight-bold">{props.question}</p>
-      <p className="text-dark-grey">{props.answer}</p>
+    <div className={isMobile ? "mt-2" : "mt-4"}>
+      <Row>
+        <Col md={11} xs={11}>
+          <p className="mono font-weight-bold">{props.question}</p>
+        </Col>
+        <Col md={1} xs={1}
+          onClick={() => setOpen(!open)}
+        >
+          {open ? <FaMinus color={`${hovColor&&!isMobile ? "#FA7815" : "#0D38D3"}`} onMouseOver={() => setHovColor(true)} onMouseOut={() => setHovColor(false)} /> : <FaPlus color={`${hovColor&&!isMobile ? "#FA7815" : "#0D38D3"}`} onMouseOver={() => setHovColor(true)} onMouseOut={() => setHovColor(false)} />}
+        </Col>
+      </Row>
+      <Collapse isOpened={open}>
+        <p className="text-dark-grey">{props.answer}</p>
+      </Collapse>
     </div>
   );
 }
