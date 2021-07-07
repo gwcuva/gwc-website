aad69b0268fc5d4973e79577f999caa7573cb852
emilybuerk <41274172+emilybuerk@users.noreply.github.com>
2021-07-06 22:25:03 -0500

    partially implemented changes to logos upon hover

--- src/components/Main/Navbar/NavContents.tsx
+++ src/components/Main/Navbar/NavContents.tsx
@@ -1,6 +1,8 @@
 import { Row } from 'react-bootstrap';
 import Discord from '../../../assets/images/discord-logo.svg';
 import Instagram from '../../../assets/images/instagram-logo.svg';
+import Discord_Blue from '../../../assets/images/discord-icon.svg';
+import Instagram_Blue from '../../../assets/images/instagram-icon.svg';
 
 interface Props {
   toggle? : () => void;
@@ -24,10 +26,16 @@ function NavContents(props: Props) {
       </div>
       <Row className={`${props.circle ? "justify-content-center" : "justify-content-end pr-3"} mt-3`}>
         <a href="https://discord.gg/D8knkzNM8E" target="_blank" rel="noreferrer noopener" onClick={props.toggle}>
-          <img src={Discord} width="28px" alt="Discord logo"/>
+          <img src={Discord} width="28px" alt="Discord logo"
+          onMouseOver={e => (e.currentTarget.src = Discord_Blue)}
+          onMouseOut={e => (e.currentTarget.src = Discord)}
+          />
         </a>
         <a href="https://instagram.com/gwcuva" target="_blank" rel="noreferrer noopener" onClick={props.toggle}>
-          <img src={Instagram} width="28px" alt="Instagram logo" className="ml-4"/>
+          <img src={Instagram} width="28px" alt="Instagram logo" className="ml-4"
+          onMouseOver={e => (e.currentTarget.src = Instagram_Blue)}
+          onMouseOut={e => (e.currentTarget.src = Instagram)}
+          />
         </a>
       </Row>
     </div>
