6b455b8d5ab486c906001b634c74d6fd62e3c303
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2021-07-22 17:29:19 -0400

    graphcms and more set up

--- src/components/GirlsHooHack/Speakers/SpeakerItem.tsx
+++ src/components/GirlsHooHack/Speakers/SpeakerItem.tsx
@@ -1,35 +1,27 @@
 import {Row, Col} from 'react-bootstrap';
-import {BrowserView, isMobile, MobileView} from 'react-device-detect';
+import {isMobile} from 'react-device-detect';
 
 interface Props {
+    key: string;
     name: string;
-    image: string;
     bio: string;
 }
 
-function SpeakerItem(props: Props) {
-    return (
-        <div>
-            <BrowserView>
-                <Row>
-                    <Col>
-                        <Row>
-                            {props.name}
-                        </Row>
-                        <Row>
-                            {props.image}
-                        </Row>
-                    </Col>
-                    <Col>
-                        <p>{props.bio}</p>
-                    </Col>
-                </Row>
-            </BrowserView>
-            <MobileView>
-
-            </MobileView>
-        </div>
-    );
+function SpeakerItem(props: Props): JSX.Element {
+    if(isMobile) {
+        return (
+            <div></div>
+        );
+    } else {
+        return (
+            <div>
+                <Col xs={4} className="mb-4">
+                    <h4 className="hack">{props.name}</h4>
+                    <p>{props.bio}</p>
+                </Col>
+            </div>
+        );
+    }
 }
 
 export default SpeakerItem;
--- src/components/GirlsHooHack/Speakers/Speakers.tsx
+++ src/components/GirlsHooHack/Speakers/Speakers.tsx
@@ -1,16 +1,54 @@
+import { useEffect, useState } from 'react';
+import { request } from 'graphql-request';
 import {Row, Col} from 'react-bootstrap';
 import {isMobile} from 'react-device-detect';
 import SpeakerItem from './SpeakerItem';
 
 function Speakers() {
+    const [speakers, setSpeakers] = useState([{'id': '', 'name': '', 'image': '', 'bio': ''}]);
+
+    useEffect(() => {
+      const fetchSpeakers = async () => {
+        const { hackathonSpeakers } = await request(
+          process.env.REACT_APP_GRAPHCMS_URL ? process.env.REACT_APP_GRAPHCMS_URL : "",
+          `
+            { 
+              hackathonSpeakers {
+                id
+                name
+                bio
+              }
+            }
+          `
+        );
+        setSpeakers(hackathonSpeakers);
+      };
+      fetchSpeakers();
+    }, []);
 
     return (
-        <Row className={`bg-hack-grey justify-content-center py-5`}>
-            <Col sm={10} xs={11} className={isMobile ? "my-3" : "my-5 py-5"}>
-                <h2 className="text-orange hack pb-3">Speakers</h2>
-                <SpeakerItem name="AAA" image="BBB" bio="CCC" />
+    <Row className={`bg-white justify-content-center py-5`}>
+        <Col sm={10} xs={11} className={isMobile ? "my-3" : "py-5"}>
+        <Row className={isMobile ? "" : "my-5"}>
+            <Col>
+            {isMobile ? 
+                <div>
+                    <h2 className="text-orange hack">PRIZES</h2>
+                    {speakers.map((speaker) => 
+                    <SpeakerItem key={speaker.id} name={speaker.name} bio={speaker.bio} ></SpeakerItem>)}
+                </div>
+                :
+                <div>
+                    <h2 className="text-orange hack pb-3">Prizes</h2>
+                    <Row>
+                    {speakers.map((speaker) => 
+                        <SpeakerItem key={speaker.id} name={speaker.name} bio={speaker.bio} ></SpeakerItem>)}
+                    </Row>
+                </div>}
             </Col>
         </Row>
+        </Col>
+    </Row>
 
     );
 }
