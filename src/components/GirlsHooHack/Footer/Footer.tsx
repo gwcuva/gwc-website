1a199e07ac945219167477c308248362316d5873
nehagb99 <neha.gb99@gmail.com>
2021-08-05 02:43:45 -0400

    removed commented code

--- src/components/GirlsHooHack/Footer/Footer.tsx
+++ src/components/GirlsHooHack/Footer/Footer.tsx
@@ -13,26 +13,6 @@ import Image from 'react-bootstrap/Image';
 function Footer() {
   return (
     <div className="bg-peacock">
-      {/*<div className={`${!isMobile && "pt-6 bg-white"}`}>
-        <div className={isMobile ? "ml-3" : "ml-4"}>
-          <img 
-            src={whiteLogo} className={`${!isMobile && "mb-4"}`} width={isMobile ? "125px" : "359px"} height={isMobile ? "39.7px" : "114px"} 
-            alt="logo" style={ isMobile ? {position:'relative', top:'75px'} : {position:'relative', zIndex: 100, top:'370px', left:'100px'} }
-          />
-          {/*<Image 
-            src={whiteLogo} className={`${!isMobile ? "" : ""} position-relative left-50`} width={isMobile ? "125px" : "359px"} 
-            height={isMobile ? "39.7px" : "114px"} alt="logo"
-          />
-          <Row style={ isMobile ? {position:'relative', zIndex: 100, top:'85px', left:'15px'} : {position:'relative', zIndex: 100, top:'370px', left:'115px'} }>
-            <a href="mailto:gwcuva@gmail.com" className="mono text-white interactive"><img src={email} className={isMobile ? "w-75 pr-1" : "mr-4"} alt="logo"/></a>
-            <a href="https://www.instagram.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={instagram} className={isMobile ? "w-75 mt-1 pr-1" : "mr-4"} alt="logo"/></a>
-            <a href="https://discord.com/invite/rKbWyuDNyG" target="_blank" rel="noreferrer noopener"><img src={discord} className={isMobile ? "w-75 mt-1 pr-1" : "mr-4"} alt="logo"/></a>
-            <a href="https://www.facebook.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={facebook} className={isMobile ? "w-75 mt-1 pr-1" : "mr-4"} alt="logo"/></a>
-            <a href="https://twitter.com/gwcuva" target="_blank" rel="noreferrer noopener"><img src={twitter} className={isMobile ? "w-75 mt-1" : "mr-4 pr-1"} alt="logo"/></a>
-          </Row>
-        </div>
-      </div>
-      <img className="w-100" src={blueWaves} alt=""/>*/}
       <img className={`${isMobile ? "footerLogoMobile" : "position-absolute footerLogoDesktop"}`} src={whiteLogo} 
         width={isMobile ? "125px" : "359px"} height={isMobile ? "39.7px" : "114px"} alt=""
       />
