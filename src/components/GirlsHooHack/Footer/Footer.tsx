41d56c44f3cf10cce88bd1b74caba7fc07155f2e
nehagb99 <neha.gb99@gmail.com>
2021-08-05 19:54:47 -0400

    added alt descriptions

--- src/components/GirlsHooHack/Footer/Footer.tsx
+++ src/components/GirlsHooHack/Footer/Footer.tsx
@@ -8,22 +8,21 @@ import facebook from '../../../assets/images/facebook-f-brands.svg';
 import twitter from '../../../assets/images/twitter-brands.svg';
 import {Row} from 'react-bootstrap';
 import {isMobile} from 'react-device-detect';
-import Image from 'react-bootstrap/Image';
  
 function Footer() {
   return (
     <div className="bg-peacock">
       <img className={`${isMobile ? "footerLogoMobile" : "position-absolute footerLogoDesktop"}`} src={whiteLogo} 
-        width={isMobile ? "125px" : "359px"} height={isMobile ? "39.7px" : "114px"} alt=""
+        width={isMobile ? "125px" : "359px"} height={isMobile ? "39.7px" : "114px"} alt="Girls Who Code at the University of Virginia logo"
       />
       <Row className={`position-absolute ${isMobile ? "footerIconRowMobile" : "footerIconRowDesktop"}`}>
-        <a href="mailto:gwcuva@gmail.com" className="mono text-white interactive"><img src={email} className={`${isMobile ? "mt-n1 footerIconMobile" : "footerIconDesktop"}`} alt="logo"/></a>
-        <a href="https://www.instagram.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={instagram} className={`${isMobile ? "footerIconMobile" : "footerIconDesktop"}`} alt="logo"/></a>
-        <a href="https://discord.com/invite/rKbWyuDNyG" target="_blank" rel="noreferrer noopener"><img src={discord} className={`${isMobile ? "footerIconMobile" : "footerIconDesktop"}`} alt="logo"/></a>
-        <a href="https://www.facebook.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={facebook} className={`${isMobile ? "footerIconMobile" : "footerIconDesktop"}`} alt="logo"/></a>
-        <a href="https://twitter.com/gwcuva" target="_blank" rel="noreferrer noopener"><img src={twitter} className={`${isMobile ? "footerIconMobile" : "footerIconDesktop"}`} alt="logo"/></a>
+        <a href="mailto:gwcuva@gmail.com" className="mono text-white interactive"><img src={email} className={`${isMobile ? "mt-n1 footerIconMobile" : "footerIconDesktop"}`} alt="Email icon"/></a>
+        <a href="https://www.instagram.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={instagram} className={`${isMobile ? "footerIconMobile" : "footerIconDesktop"}`} alt="Instagram icon"/></a>
+        <a href="https://discord.com/invite/rKbWyuDNyG" target="_blank" rel="noreferrer noopener"><img src={discord} className={`${isMobile ? "footerIconMobile" : "footerIconDesktop"}`} alt="Discord icon"/></a>
+        <a href="https://www.facebook.com/gwcuva/" target="_blank" rel="noreferrer noopener"><img src={facebook} className={`${isMobile ? "footerIconMobile" : "footerIconDesktop"}`} alt="Facebook icon"/></a>
+        <a href="https://twitter.com/gwcuva" target="_blank" rel="noreferrer noopener"><img src={twitter} className={`${isMobile ? "footerIconMobile" : "footerIconDesktop"}`} alt="Twitter icon"/></a>
       </Row>
-      <img className="w-100 position-relative" src={blueWaves} alt=""/>
+      <img className="w-100 position-relative" src={blueWaves} alt="Blue waves"/>
     </div>
   );
 }
