d83a11760892c7823640d780189c40b08ecd972b
nehagb99 <neha.gb99@gmail.com>
2021-09-11 20:19:49 -0400

    Resized iFrame

--- src/components/GirlsHooHack/SponsorshipProspectus/SponsorshipProspectus.tsx
+++ src/components/GirlsHooHack/SponsorshipProspectus/SponsorshipProspectus.tsx
@@ -9,10 +9,10 @@ function SponsorshipProspectus() {
     <Row className={`bg-hack-grey justify-content-center py-5`}>
       <Col sm={10} xs={11} className={isMobile ? "my-3" : "my-5 py-5"}>
         <h2 className="text-orange hack">Sponsorship Prospectus</h2>
-        <Row className={isMobile ? "" : "my-5"}>
+        <Row className={isMobile ? "" : "mt-4 mb-5"}>
           <Col md={10} xs={11} className={isMobile ? "my-3" : "mr-5"}>
             <h3 className="mono text-peach mt-4 mb-4">Interested in sponsoring us? Check out our Sponsorship Prospectus!</h3>
-            <iframe className="sponsorshipProspectus" title="Sponsorship Prospectus" src="https://cdn.kutasoftware.com/Worksheets/Calc/01%20-%20Limits%20by%20Direct%20Evaluation.pdf"></iframe>
+            <iframe className="sponsorshipProspectus" title="Sponsorship Prospectus" src="https://girls-hoo-hack.webnode.com/_files/200000013-94ee994eec/GHH%202020%20Sponsorship%20Prospectus-7.pdf"></iframe>
           </Col>
         </Row>
       </Col>
