81c73c71c0328b181eaefd6421167b045533ea1e
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2021-10-01 10:55:20 -0400

    removed unused import statement

--- src/components/GirlsHooHack/GirlsHooHack.tsx
+++ src/components/GirlsHooHack/GirlsHooHack.tsx
@@ -9,7 +9,6 @@ import MobileHeader from './Header/MobileHeader';
 //import Schedule from './Schedule/Schedule';
 //import Sponsors from './Sponsors/Sponsors';
 import Speakers from './Speakers/Speakers';
-//import Panelists from './Panelists/Panelists';
 import {BrowserView, MobileView} from 'react-device-detect';
 import Waves from '../../assets/images/hack-header-mobile-wave.svg';
 
