e066091effc977bffc241834b57ed2d615c89534
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2021-10-17 16:29:10 -0400

    Lgz9cm/ghhoverflow (#131)
    * removed horizontal overflow on desktop and mobile
    
    * installed missing module
    
    * Clean up import statements and revert commit
    
    Co-authored-by: pragnya17 <59893188+pragnya17@users.noreply.github.com>

--- src/components/GirlsHooHack/GirlsHooHack.tsx
+++ src/components/GirlsHooHack/GirlsHooHack.tsx
@@ -1,17 +1,14 @@
-//import React from 'react';
-import About from './About/About';
-import Footer from './Footer/Footer';
-import FAQ from './FAQ/FAQ';
 import Header from './Header/Header';
 import MobileHeader from './Header/MobileHeader';
+import About from './About/About';
 import JumpStart from './JumpStart/JumpStart';
-import Prizes from './Prizes/Prizes';
 import Schedule from './Schedule/Schedule';
+import FAQ from './FAQ/FAQ';
 import Sponsors from './Sponsors/Sponsors';
-import Speakers from './Speakers/Speakers';
-import SponsorshipProspectus from './SponsorshipProspectus/SponsorshipProspectus';
+//import Prizes from './Prizes/Prizes';
 import {BrowserView, MobileView} from 'react-device-detect';
 import Waves from '../../assets/images/hack-header-mobile-wave.svg';
+import Footer from './Footer/Footer';
 
 function GirlsHooHack() {
   return (
@@ -23,15 +20,14 @@ function GirlsHooHack() {
         <MobileHeader id="MobileHeader"/>
         <img src={Waves} style={{marginTop:"-215px", marginBottom:"-120px"}} width = "100%" alt="Orange wave" />
       </MobileView>
-      <About />
-      <Speakers />
-      <JumpStart />
-      <Schedule />
-      <Prizes />
-      <FAQ />
-      <Sponsors />
-      <SponsorshipProspectus />
-      <Footer />
+      <div  className="overflow-hidden">
+        <About />
+        <JumpStart />
+        <Schedule />
+        <FAQ />
+        <Sponsors />
+        <Footer />
+      </div>
     </div>
   );
 }
