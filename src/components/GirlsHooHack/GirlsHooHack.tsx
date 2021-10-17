97834318b83f7eaf3461d49927b139756897d612
pragnya17 <59893188+pragnya17@users.noreply.github.com>
2021-10-17 17:16:54 -0400

    Add speakers (#136)

--- src/components/GirlsHooHack/GirlsHooHack.tsx
+++ src/components/GirlsHooHack/GirlsHooHack.tsx
@@ -4,6 +4,7 @@ import About from './About/About';
 import JumpStart from './JumpStart/JumpStart';
 //import Prizes from './Prizes/Prizes';
 import Schedule from './Schedule/Schedule';
+import Speakers from './Speakers/Speakers'
 import FAQ from './FAQ/FAQ';
 import Sponsors from './Sponsors/Sponsors';
 import SponsorshipProspectus from './SponsorshipProspectus/SponsorshipProspectus';
@@ -26,6 +27,7 @@ function GirlsHooHack() {
         <About />
         <JumpStart />
         <Schedule />
+        <Speakers />
         <FAQ />
         <Sponsors />
         <SponsorshipProspectus />
