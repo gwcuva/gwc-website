657f9dca2800813dd0c118d25b0e1d7694e4d0d2
pragnya17 <pp8sdv@virginia.edu>
2022-01-20 21:59:22 -0500

    remove unused imports

--- src/components/Main/Navbar/Navbar.tsx
+++ src/components/Main/Navbar/Navbar.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import {Col, Row} from 'react-bootstrap'
+import {Row} from 'react-bootstrap'
 import Logo from '../../../assets/images/peacock-logo.svg'
 
 function Navbar() {
