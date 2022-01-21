ca9489aa09b2a8851bd211f6cc601fafc2c97cb5
pragnya17 <pp8sdv@virginia.edu>
2022-01-20 21:04:40 -0500

    Update links

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -6,6 +6,7 @@ function NotFound() {
       <h1 className="text-peacock">Page Not Found</h1>
       <a href="/"><h2 className="text-orange">Go to Main Page</h2></a>
       <a href="/hack"><h2 className="text-orange">Go to Hackathon Page</h2></a>
+      <a href="/careerfair"><h2 className="text-orange">Go to Career Fair Page</h2></a>
     </div>
   );
 }
