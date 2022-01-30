62343ab700a482a04978d53e06fb72a6669459a5
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2022-01-30 14:16:14 -0500

    added and connected career fair schedule to graph cms

--- src/components/CareerFair/Schedule/Activity.tsx
+++ src/components/CareerFair/Schedule/Activity.tsx
@@ -11,12 +11,14 @@ interface Props {
 function Activity(props: Props): JSX.Element {
   return (
     <Row className="mb-2">
-        <Col>
+        <Col xs={2}>
             <p className="text-dark-grey mono">{props.time}</p>
         </Col>
-        <Col>
+        <Col xs={4}>
             <p className="mono font-weight-bold mb-0">{props.name}</p>
             {props.detail && <p className="text-dark-grey interactive mb-0">{props.detail}</p>}
+        </Col>
+        <Col>
             {props.link && <a href={props.link} target="_blank" rel="noreferrer" className="text-blue interactive mb-0"><u>{props.linkname}</u> &#8599;</a>}
         </Col>
     </Row>
