ea156437f2b24017bb3d95c37c2e315c9048c08a
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2022-01-31 04:12:02 -0500

    added and connected career fair schedule to graph cms (#162)
    * added and connected career fair schedule to graph cms
    
    * added in person meeting to code

--- src/components/CareerFair/Schedule/Activity.tsx
+++ src/components/CareerFair/Schedule/Activity.tsx
@@ -6,18 +6,22 @@ interface Props {
   detail?: string;
   linkname?: string;
   link?: string;
+  inperson?: string;
 }
 
 function Activity(props: Props): JSX.Element {
   return (
     <Row className="mb-2">
-        <Col>
+        <Col xs={2}>
             <p className="text-dark-grey mono">{props.time}</p>
         </Col>
-        <Col>
+        <Col xs={4}>
             <p className="mono font-weight-bold mb-0">{props.name}</p>
             {props.detail && <p className="text-dark-grey interactive mb-0">{props.detail}</p>}
+        </Col>
+        <Col>
             {props.link && <a href={props.link} target="_blank" rel="noreferrer" className="text-blue interactive mb-0"><u>{props.linkname}</u> &#8599;</a>}
+            {props.inperson && <p className="text-dark-grey interactive mb-0">{props.inperson}</p>}
         </Col>
     </Row>
   );
