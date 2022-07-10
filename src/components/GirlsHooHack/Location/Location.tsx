56d4bc22df9245ad2e59c0e5f1d301b7003d2411
hannahlee8 <92696948+hannahlee8@users.noreply.github.com>
2022-07-10 18:57:56 -0400

    Format Location GHH (#184)
    Align Location subsection with other subsections

--- src/components/GirlsHooHack/Location/Location.tsx
+++ src/components/GirlsHooHack/Location/Location.tsx
@@ -6,7 +6,7 @@ function Location() {
   return (
     <div>
         <Row className="bg-hack-grey justify-content-center pb-5 pt-5">
-            <Col className={isMobile ? "ml-3" : "pl-5 ml-5"}>
+            <Col sm={10} xs={11} className={isMobile ? "ml-3" : "pl-2 ml-2"}>
                 <h2 className="text-orange font-weight-bold pb-3">Location</h2>
                 <p className="mono font-weight-bold">3 Elliewood, Charlottesville, 22903</p>
             </Col>
