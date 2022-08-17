448f4e496b659ce0225cc04006a0e110ac75e276
Hao Liu <gbp9yr@virginia.edu>
2022-08-17 09:15:03 -0400

    Revert "Revert "Merge branch 'main' into gbp9yr-footer""
    This reverts commit b7eb81b135db4994f8af415469f94e4d876d6e8d.

---
+++ src/components/GirlsHooHack2022/GirlsHooHack.tsx
@@ -0,0 +1,46 @@
+import Header from './Header/Header';
+import NavbarGHH from './NavbarGHH/NavbarGHH';
+import MobileNavbarGHH from './NavbarGHH/MobileNavbarGHHs';
+import MobileHeader from './Header/MobileHeader';
+import About from './About/About';
+import JumpStart from './JumpStart/JumpStart';
+//import Prizes from './Prizes/Prizes';
+import Schedule from './Schedule/Schedule';
+import Speakers from './Speakers/Speakers';
+import Location from './Location/Location';
+import FAQ from './FAQ/FAQ';
+import Sponsors from './Sponsors/Sponsors';
+import SponsorshipProspectus from './SponsorshipProspectus/SponsorshipProspectus';
+//import Prizes from './Prizes/Prizes';
+import {BrowserView, MobileView} from 'react-device-detect';
+import Waves from '../../assets/images/hack-header-mobile-wave.svg';
+import Footer from './Footer/Footer';
+
+function GirlsHooHack() {
+  return (
+    <div className="overflow-auto">
+      <BrowserView>
+        <NavbarGHH />
+        <Header id="Header"/>
+      </BrowserView>
+      <MobileView>
+        <MobileNavbarGHH />
+        <MobileHeader id="MobileHeader"/>
+        <img src={Waves} style={{marginTop:"-215px", marginBottom:"-120px"}} width = "100%" alt="Orange wave" />
+      </MobileView>
+      <div  className="overflow-hidden">
+        <About />
+        <JumpStart />
+        <Schedule />
+        <Speakers />
+        <Location />
+        <FAQ />
+        <Sponsors />
+        <SponsorshipProspectus />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default GirlsHooHack;
