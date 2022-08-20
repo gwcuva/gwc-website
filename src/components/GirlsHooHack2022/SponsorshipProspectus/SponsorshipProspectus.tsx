f51ff1dca84c31b8426b68d8a717c4cf4a08335b
rushvipatel <72409300+rushvipatel@users.noreply.github.com>
2022-08-19 22:00:51 -0400

    Update SponsorshipProspectus.tsx (#200)
    * Update SponsorshipProspectus.tsx
    
    moved to graphcms
    
    * Update SponsorshipProspectus.tsx
    
    Co-authored-by: nehagb99 <neha.gb99@gmail.com>

--- src/components/GirlsHooHack2022/SponsorshipProspectus/SponsorshipProspectus.tsx
+++ src/components/GirlsHooHack2022/SponsorshipProspectus/SponsorshipProspectus.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import {Row, Col} from 'react-bootstrap';
 import { isMobile } from 'react-device-detect';
+import { request } from 'graphql-request';
 
 function SponsorshipProspectus(): JSX.Element {
+  const [link, setLink] = useState({'year': 2022, 'url': ''});
+
+  useEffect(() => {
+    const fetchLink = async () => {
+      const { hackathonProspectus } = await request(
+        process.env.REACT_APP_GRAPHCMS_URL ? process.env.REACT_APP_GRAPHCMS_URL : "",
+        `
+          { 
+            hackathonProspectus(where: {id: "cl6wzrs102i4u0biz171xcdgs"}) {
+              url
+              year
+            }
+          }
+        `
+      );
+      setLink(hackathonProspectus);
+    };
+
+    fetchLink();
+  }, []);
+
 
   return (
     <Row className={`bg-hack-grey justify-content-center py-5`}>
@@ -10,14 +32,14 @@ function SponsorshipProspectus(): JSX.Element {
         <h2 className="text-orange hack">{isMobile ? "SPONSORSHIP PROSPECTUS" : "Sponsorship Prospectus"}</h2>
         <Row className={isMobile ? "" : "mt-4 mb-5"}>
           <Col md={10} xs={11} className={isMobile ? "my-3" : "mr-5"}>
-            <p>Coming Soon!</p>
-            {/*<h3 className="mono text-peach mt-4 mb-4" white-space="pre">Interested in sponsoring us? Check out our&nbsp;
-              <a href="https://drive.google.com/file/d/1zCGhSBKaAHCGX-zT2KnA7yCuNUpgtfoN/view" 
+            {link ?
+            <h3 className="mono text-peach mt-4 mb-4" white-space="pre">Interested in sponsoring us? Check out our&nbsp;
+              <a href={link.url}
                 target="_blank" rel="noreferrer noopener" className="hoverColor">
                 Sponsorship Prospectus
-              </a>
-              !
-  </h3>*/}
+              </a>!
+            </h3> : 
+            <p>Coming Soon!</p>}
           </Col>
         </Row>
       </Col>
@@ -26,3 +48,4 @@ function SponsorshipProspectus(): JSX.Element {
 }
 
 export default SponsorshipProspectus;
+
