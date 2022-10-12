00d3e8887b2b51933194d0f0dac163534bd14780
Rushvi Patel <72409300+rushvipatel@users.noreply.github.com>
2022-10-12 15:30:13 -0400

    added

--- src/components/CareerFair/Signup/Signup.tsx
+++ src/components/CareerFair/Signup/Signup.tsx
@@ -1,18 +1,41 @@
+import { useEffect, useState } from 'react';
 import {Row, Col} from 'react-bootstrap';
 import { isMobile } from 'react-device-detect';
+import { request } from 'graphql-request';
 
 
-function Location() {
+
+function Registration() {
+  const [registration, setRegistration] = useState({'registrationLink': '','signUpLink': ''});
+
+  useEffect(() => {
+    const fetchRegistration= async () => {
+      const { careerFairRegistration } = await request(
+        process.env.REACT_APP_GRAPHCMS_URL ? process.env.REACT_APP_GRAPHCMS_URL : "",
+        `
+          { 
+            careerFairRegistration(where: {year: 2022}) {
+              signUpLink
+              registrationLink
+            }
+          }
+        `
+      );
+      setRegistration(careerFairRegistration);
+    };
+
+    fetchRegistration();
+  }, []);
   return (
     <div>
         <Row className="justify-content-center pb-5 pt-5 bg-hack-grey">
             <Col sm={10} xs={11} className={isMobile ? "my-3" : "my-5 py-5"}>
                 <h2 className="text-orange font-weight-bold pb-3">Registration</h2>
                 <h3 className="mono text-peach hack mb-4"> <a className="mono text-peach hack mb-4"
-                 href="https://forms.gle/JbkndWdHQCLwDuo68" target="_blank" rel="noreferrer noopener"> <u>
+                 href={registration.registrationLink} target="_blank" rel="noreferrer noopener"> <u>
                    REGISTER</u> </a> to save your spot and submit a resume </h3>
                    <h3 className="mono text-peach hack mb-4"> <a className="mono text-peach hack mb-4"
-                 href="https://www.signupgenius.com/go/10C0E4DADAD2FA5F9CE9-signupcf" target="_blank" rel="noreferrer noopener"> <u>
+                 href={registration.signUpLink} target="_blank" rel="noreferrer noopener"> <u>
                    SIGN UP</u> </a> for career panels and 1:1 sessions </h3> 
             </Col>
         </Row>
@@ -20,4 +43,4 @@ function Location() {
   );
 }
 
-export default Location;
+export default Registration;
