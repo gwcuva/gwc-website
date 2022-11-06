fec3009062d9e466145a89b29e61c780f463af7f
Rushvi Patel <72409300+rushvipatel@users.noreply.github.com>
2022-11-06 16:47:25 -0500

    updated

--- src/components/CareerFair/Header/Header.tsx
+++ src/components/CareerFair/Header/Header.tsx
@@ -2,6 +2,8 @@ import {Row, Col} from 'react-bootstrap';
 import {isMobile} from 'react-device-detect';
 import Waves from '../../../assets/images/hack-header-desktop-wave.svg';
 import UVA from '../../../assets/images/uva.svg';
+import { useEffect, useState } from 'react';
+import { request } from 'graphql-request';
 
 interface Props {
   toggle? : () => void;
@@ -9,6 +11,35 @@ interface Props {
 }
 
 function Header(props: Props) {
+  const [header, setHeader] = useState({'year': 2022, 'month': '', 'day': '', 'format': '', 'registration': '', 'time': ''});
+
+  useEffect(() => {
+    const fetchHeader = async () => {
+      const { careerFairHeader } = await request(
+        process.env.REACT_APP_GRAPHCMS_URL ? process.env.REACT_APP_GRAPHCMS_URL : "",
+        `
+          { 
+            careerFairHeader(where:{year:2022}) {
+              year
+              month
+              day
+              format
+              time
+              registration
+            }
+          }
+        `
+      );
+      setHeader(careerFairHeader);
+    };
+
+    fetchHeader();
+  }, []);
+
+
+
+
+
   return (
     <div id={props.id} className="container-fluid m-0 bg-turq" >
         <img className="w-100 position-absolute ml-n3 mt-4" src={Waves} alt="Orange waves"/>
@@ -24,17 +55,17 @@ function Header(props: Props) {
 
             <Col xs={3}>
               <img src={UVA} alt="UVA text"/>
-              <h3 className="text-blue"> February 11th</h3>
-              <h3 className="text-blue"> 11am - 3pm</h3>
+              <h3 className="text-blue"> {header.month} {header.day}</h3>
+              <h3 className="text-blue"> {header.time}</h3>
             </Col>
         </Row>
 
         <Row xs={12} className="mt-n5">
           <Col xs={1}></Col>
           <Col xs={3} className="text-white"><h1>HOO</h1></Col>
-          <Col xs={4}><h2 className={isMobile ? "text-blue":"text-blue font-weight-bold pt-4"}>2022 HYBRID CAREER FAIR</h2></Col>
+          <Col xs={4}><h2 className={isMobile ? "text-blue":"text-blue font-weight-bold pt-4"}>{header.year} {header.format} CAREER FAIR</h2></Col>
           <Col xs={1}></Col>
-          <Col xs={3} className="h1 pl-4 pt-4"> <a className="text-white font-weight-bold" href="https://forms.gle/JbkndWdHQCLwDuo68" target="_blank" rel="noreferrer noopener"> <u>REGISTER</u> </a> </Col>
+          <Col xs={3} className="h1 pl-4 pt-4"> <a className="text-white font-weight-bold" href={header.registration} target="_blank" rel="noreferrer noopener"> <u>REGISTER</u> </a> </Col>
         </Row>
 
         <Row className="pt-0 mt-n5 pb-5">
