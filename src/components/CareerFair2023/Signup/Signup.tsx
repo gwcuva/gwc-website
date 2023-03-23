de0a63cc1c3d80efd19c6d8475eeca0be7b10ecd
hannahlee8 <hsl8xrv@virginia.edu>
2023-03-23 18:50:02 -0400

    move registration to hygraph

--- src/components/CareerFair2023/Signup/Signup.tsx
+++ src/components/CareerFair2023/Signup/Signup.tsx
@@ -1,15 +1,36 @@
+import { useEffect, useState } from 'react';
 import {Row, Col} from 'react-bootstrap';
 import { isMobile } from 'react-device-detect';
+import { request } from 'graphql-request';
 
 
-function Location() {
+function Signup() {
+  const [registration, setRegistration] = useState({'registrationLink': ''});
+  useEffect(() => {
+    const fetchRegistration = async () => {
+      const { careerFairRegistration } = await request(
+        process.env.REACT_APP_GRAPHCMS_URL ? process.env.REACT_APP_GRAPHCMS_URL : "",
+        `
+          { 
+            careerFairRegistration(where: {year: 2023}) {
+              registrationLink
+            }
+          }
+        `
+      );
+      setRegistration(careerFairRegistration);
+    };
+
+    fetchRegistration();
+  }, []);
+
   return (
     <div>
         <Row className="justify-content-center pb-5 pt-5 bg-hack-grey" id = "CFRegistration">
             <Col sm={10} xs={11} className={isMobile ? "my-3" : "my-5 py-5"}>
                 <h2 className="text-orange font-weight-bold pb-3">Registration</h2>
                 <h3 className="mono text-peach hack mb-4"> <a className="mono text-peach hack mb-4"
-                 href="https://forms.gle/tDtGrw5dkKPNB8LG9" target="_blank" rel="noreferrer noopener"> <u>
+                    href={registration.registrationLink} target="_blank" rel="noreferrer noopener"> <u>
                    REGISTER</u> </a> to save your spot and submit a resume </h3>
             </Col>
         </Row>
@@ -17,4 +38,4 @@ function Location() {
   );
 }
 
-export default Location;
+export default Signup;
