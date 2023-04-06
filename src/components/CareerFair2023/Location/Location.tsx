059b92cab4541aa45300ef8faf8ab2234d4a324d
nehagb99 <neha.gb99@gmail.com>
2023-04-06 19:42:03 -0400

    updated to be for 2023

--- src/components/CareerFair2023/Location/Location.tsx
+++ src/components/CareerFair2023/Location/Location.tsx
@@ -1,14 +1,42 @@
+import { useEffect, useState } from 'react';
 import {Row, Col} from 'react-bootstrap';
 import { isMobile } from 'react-device-detect';
-
+import { request } from 'graphql-request';
 
 function Location() {
+  
+  const [location, setLocation] = useState({'virtual': '', 'inPerson': '', 'id': ''});
+
+  useEffect(() => {
+    const fetchLocation = async () => {
+      const { careerFairLocation } = await request(
+        process.env.REACT_APP_GRAPHCMS_URL ? process.env.REACT_APP_GRAPHCMS_URL : "",
+        `
+          { 
+            careerFairLocation(where: {year: 2023}) {
+              id
+              virtual
+              inPerson
+            }
+          }
+        `
+      );
+      setLocation(careerFairLocation);
+    };
+    fetchLocation();
+  }, []);
+
   return (
-    <div>
-        <Row className="justify-content-center pb-5 pt-5" id="CFLocation">
-            <Col sm={10} xs={11} className={isMobile ? "my-3" : "my-5 py-5"}>
-                <h2 className="text-orange font-weight-bold pb-3">Location</h2>
-                <h3 className="mono text-peach hack mb-4">Ern Commons at 567 McCormick Rd</h3>
+    <div id="Location">
+        <Row className="bg-white justify-content-center pb-5 pt-5">
+            <Col sm={10} xs={11} className={isMobile ? "ml-3" : "pl-2 ml-2"}>
+                <h2 className="text-orange font-weight-bold pb-3">{isMobile ? "LOCATION" : "Location"}</h2>
+                {location ?
+                  <><h3 className="mono text-peach hack mb-4">{location.inPerson}</h3>
+                  {location.virtual && <h3 className="mono text-peach hack mb-4">{location.virtual}</h3>}</> 
+                  :
+                  <h3>Coming Soon!</h3>
+                } 
             </Col>
         </Row>
     </div>
