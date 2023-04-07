a2c4c7b5391c43b99ce8087569b2b5d84fe70692
rushvipatel <72409300+rushvipatel@users.noreply.github.com>
2023-04-06 20:15:00 -0400

    Headerto hygraph umm9ef (#237)
    * discord invite to binary blue on hover
    
    * updated
    
    * changed to 2023
    
    ---------
    
    Co-authored-by: nehagb99 <neha.gb99@gmail.com>

--- src/components/CareerFair2022/Header/MobileHeader.tsx
+++ src/components/CareerFair2022/Header/MobileHeader.tsx
@@ -1,4 +1,6 @@
 import {Row, Col} from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { request } from 'graphql-request';
 
 interface Props {
     toggle? : () => void;
@@ -6,11 +8,35 @@ interface Props {
 }
 
 function MobileHeader(props: Props) {
+    const [header, setHeader] = useState({'year': 2022, 'month': '', 'day': '', 'format': '', 'registration': '', 'time': ''});
+
+  useEffect(() => {
+    const fetchHeader = async () => {
+      const { careerFairHeader } = await request(
+        process.env.REACT_APP_GRAPHCMS_URL ? process.env.REACT_APP_GRAPHCMS_URL : "",
+        `
+          { 
+            careerFairHeader(where:{year:2022}) {
+              year
+              month
+              day
+              format
+              time
+              registration
+            }
+          }
+        `
+      );
+      setHeader(careerFairHeader);
+    };
+
+    fetchHeader();
+  }, []);
     return(
         <div id={props.id} className="container-fluid bg-turq">
             <meta id="viewport" name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"></meta>
             <Row className="mono text-blue pt-5 pl-4 pb-n5" style={{fontSize:13.5}}>
-                <text><big>2022 Hybrid Career Fair</big></text>
+                <text><big>{header.year} {header.format} Career Fair</big></text>
             </Row>
 
             <Row xs={8} className="text-white pl-3 pt-n5">
@@ -26,13 +52,13 @@ function MobileHeader(props: Props) {
             </Row>
 
             <Row xs={10} className="text-blue pl-3 pt-n5 justify-content-between align-items-center">
-                <h3>February 11th</h3>
-                <Col xs={5} className="h3"> <a className="text-white font-weight-bold" href="https://forms.gle/JbkndWdHQCLwDuo68" target="_blank" rel="noreferrer noopener"> <u>REGISTER</u> </a> </Col>
+                <h3>{header.month} {header.day}</h3>
+                <Col xs={5} className="h3"> <a className="text-white font-weight-bold" href={header.registration} target="_blank" rel="noreferrer noopener"> <u>REGISTER</u> </a> </Col>
             </Row>
 
 
             <Row xs={10} className="text-blue pl-3 pt-n5">
-                <h3>11am - 3pm</h3>                
+                <h3>{header.time}</h3>                
             </Row>
 
             <Row xs={12} className="pt-5"></Row>
