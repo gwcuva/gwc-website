ced41de0ea860d9088a7f333f7e281dc337b773e
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2023-04-08 12:56:24 -0400

    updated footer

--- src/components/GirlsHooHack2022/Footer/Footer.tsx
+++ src/components/GirlsHooHack2022/Footer/Footer.tsx
@@ -94,9 +94,6 @@ function Footer() {
           GHH 2021 Website: 
           <a href="/hack2021" target="_blank" rel="noreferrer noopener" className="footerArchiveMobile">hack2021</a>
       </Row>}
-      {isMobile && <Row className="footerArchiveMobileRow"> 
-        Although this organization has members who are University of Virginia students and may have, University employees associated or engaged in its activities and affairs, the organization is not a part of or an agency. It is a separate and independent organization, which is responsible for and manages its own activities and affairs. The University does not direct, supervise or control the organization and is not responsible for the organization’s contracts, acts or omissions.
-      </Row>}
       <img className="w-100 position-relative" src={blueWaves} alt="Blue waves"/>
     </div>
   );
