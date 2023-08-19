68fa60cac6b49333951dc81ad56ac1e9c2449f14
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2023-08-18 21:09:38 -0400

    updated with correct podcast sites and logos

--- src/components/Main/Podcast/Podcast.tsx
+++ src/components/Main/Podcast/Podcast.tsx
@@ -3,6 +3,10 @@ import { request } from 'graphql-request';
 import {Container, Row, Col} from 'react-bootstrap';
 import Arrow from '../../../assets/images/arrow-right.svg';
 import PodcastImage from '../../../assets/images/podcast-image.jpeg';
+import GooglePodcastsLogo from '../../../assets/images/google-podcasts-logo.png';
+import PocketCastsPodcastLogo from '../../../assets/images/pocketcasts-podcast-logo.webp';
+import RadioPublicPodcastLogo from '../../../assets/images/radiopublic-podcast-logo.png';
+import SpotifyPodcastLogo from '../../../assets/images/spotify-podcast-logo.webp';
 import { isMobile } from 'react-device-detect';
 
 interface Props {
@@ -11,7 +15,7 @@ interface Props {
 
 function Podcast(props: Props) {
     return (
-        <div id={props.id}>
+        <div id={props.id} className="pb-5">
             <Row className="justify-content-center">
                 <Col lg={6} md={8} sm={10} xs={11}>
                     <h3 className="text-turq">Stay Connected</h3>
@@ -29,29 +33,21 @@ function Podcast(props: Props) {
                     <img src={PodcastImage} width={isMobile ? "140px" : "257px"} alt="title cover of podcast" />
                 </Col>
                 <Col lg={3} md={4} sm={5} xs={5} className="justify-content-center">
-                    <Row className="justify-content-center">WHERE TO LISTEN</Row>
-                    <Row className="mt-2">
+                    <Row className="justify-content-center font-weight-bold mt-4">WHERE TO LISTEN</Row>
+                    <Row className="justify-content-center mt-2">
                         <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy81NjM0OTM3OC9wb2RjYXN0L3Jzcw==">
-                            <img src={PodcastImage} width={isMobile ? "30px" : "75px"} alt="google podcast logo linked to gwc podcast on that platform" className="mr-2" />
-                        </a>
-                        <a href="https://pca.st/sv13qma1">
-                            <img src={PodcastImage} width={isMobile ? "30px" : "75px"} alt="pocket cast logo linked to gwc podcast on that platform" className="mr-2" />
-                        </a>
-                        <a href="https://radiopublic.com/girls-hoo-code-the-podcast-WdnRkQ">
-                            <img src={PodcastImage} width={isMobile ? "30px" : "75px"} alt="radiopublic logo linked to gwc podcast on that platform" className="mr-2" />
+                            <img src={GooglePodcastsLogo} width={isMobile ? "30px" : "75px"} alt="google podcast logo linked to gwc podcast on that platform" className="circleImg mr-2" />
                         </a>
                         <a href="https://open.spotify.com/show/2gX38ig5BhHgdzY8NT4uZ6">
-                            <img src={PodcastImage} width={isMobile ? "30px" : "75px"} alt="spotify logo linked to gwc podcast on that platform" className="mr-2" />
+                            <img src={SpotifyPodcastLogo} width={isMobile ? "30px" : "75px"} alt="spotify logo linked to gwc podcast on that platform" className="mr-2" />
                         </a>
-                        
-                    
-                    </Row>
-                    <Row className="mt-2">
+                        <a href="https://pca.st/sv13qma1">
+                            <img src={PocketCastsPodcastLogo} width={isMobile ? "30px" : "75px"} alt="pocket cast logo linked to gwc podcast on that platform" className="mr-2" />
+                        </a>
                         <a href="https://radiopublic.com/girls-hoo-code-the-podcast-WdnRkQ">
-                            <img src={PodcastImage} width={isMobile ? "30px" : "75px"} alt="copy rss logo linked to gwc podcast on that platform" className="mr-2" />
+                            <img src={RadioPublicPodcastLogo} width={isMobile ? "30px" : "75px"} alt="radiopublic logo linked to gwc podcast on that platform" className="mr-2" />
                         </a>
                     </Row>
-                        
                 </Col>
             </Row>
         </div>
