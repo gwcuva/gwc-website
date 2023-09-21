cea757d5af72681240fb3d5e8bd30c501d69969a
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2023-09-21 00:45:22 -0400

    added sponsors, fixed location, fixed about description

--- src/components/GirlsHooHack2023/Sponsors/Sponsors.tsx
+++ src/components/GirlsHooHack2023/Sponsors/Sponsors.tsx
@@ -1,33 +1,32 @@
-// import React , { useEffect, useState } from 'react';
-import React , { useEffect } from 'react';
-// import { request } from 'graphql-request';
+import React , { useEffect, useState } from 'react';
+import { request } from 'graphql-request';
 import {Row, Col} from 'react-bootstrap';
 import {isMobile} from 'react-device-detect';
-// import Image from 'react-bootstrap/Image';
+import Image from 'react-bootstrap/Image';
 
 function Sponsors() {
 
-  // const [sponsors, setSponsors] = useState([{'level': '', 'sponsorImage': {'url': ''}, 'sponsorName': '', 'sponsorUrl': '', 'size': ''}]);
+  const [sponsors, setSponsors] = useState([{'level': '', 'sponsorImage': {'url': ''}, 'sponsorName': '', 'sponsorUrl': '', 'size': ''}]);
 
   useEffect(() => {
     const fetchSponsors = async () => {
-      // const { hackathonSponsors } = await request(
-      //   process.env.REACT_APP_GRAPHCMS_URL ? process.env.REACT_APP_GRAPHCMS_URL : "",
-      //   `
-      //     { 
-      //       hackathonSponsors(where: { year: 2022 }) {
-      //         level
-      //         sponsorImage {
-      //           url
-      //         }
-      //         sponsorName
-      //         sponsorUrl
-      //         size
-      //       }
-      //     }
-      //   `
-      // );
-      // setSponsors(hackathonSponsors);
+      const { hackathonSponsors } = await request(
+        process.env.REACT_APP_GRAPHCMS_URL ? process.env.REACT_APP_GRAPHCMS_URL : "",
+        `
+          { 
+            hackathonSponsors(where: { year: 2023 }) {
+              level
+              sponsorImage {
+                url
+              }
+              sponsorName
+              sponsorUrl
+              size
+            }
+          }
+        `
+      );
+      setSponsors(hackathonSponsors);
     };
 
     fetchSponsors();
@@ -36,12 +35,12 @@ function Sponsors() {
   // const goldsponsors = sponsors.filter( function (s) {
   //   return s.level === "Gold"
   // });
-  // const silversponsors = sponsors.filter( function (s) {
-  //   return s.level === "Silver"
-  // });
-  // const bronzesponsors = sponsors.filter( function (s) {
-  //   return s.level === "Bronze"
-  // });
+  const silversponsors = sponsors.filter( function (s) {
+    return s.level === "Silver"
+  });
+  const bronzesponsors = sponsors.filter( function (s) {
+    return s.level === "Bronze"
+  });
   // const othersponsors = sponsors.filter( function (s) {
   //   return (s.level !== "Gold" && s.level !=="Silver" && s.level !== "Bronze");
   // });
@@ -50,8 +49,8 @@ function Sponsors() {
     <Row className="justify-content-center bg-white py-5" id="Sponsors">
       <Col sm={10} xs={11} className={isMobile ? "my-3" : "my-5 py-5"}>
         <h2 className="text-orange hack">{isMobile ? "SPONSORS" : "Sponsors"}</h2>
-        <h3 className="mono text-peach hack mt-5">Gold Sponsors</h3>
-        <p>Coming Soon!</p>
+        {/* <h3 className="mono text-peach hack mt-5">Gold Sponsors</h3>
+        <p>Coming Soon!</p> */}
         {/* {goldsponsors.length===0 && silversponsors.length===0 && othersponsors.length===0 &&
           <p>Coming Soon!</p>
         }
@@ -75,9 +74,9 @@ function Sponsors() {
           )}
         </Row>
         } */}
-        <h3 className="mono text-peach hack mt-5">Silver Sponsors</h3>
-        <p>Coming Soon!</p>
-        {/* {silversponsors.length>0 && <h3 className="mono text-peach hack mt-2">Silver Sponsors</h3>}
+        {/* <h3 className="mono text-peach hack mt-5">Silver Sponsors</h3> */}
+        {/* <p>Coming Soon!</p> */}
+        {silversponsors.length>0 && <h3 className="mono text-peach hack mt-2">Silver Sponsors</h3>}
         {silversponsors.length>0 &&
         <Row className={`${!isMobile && "mt-1 mb-5"} d-flex justify-content-around`}>
           {silversponsors.map((sponsor) => 
@@ -96,10 +95,10 @@ function Sponsors() {
             </Col>
           )}
         </Row>
-        } */}
-        <h3 className="mono text-peach hack mt-5">Bronze Sponsors</h3>
-        <p>Coming Soon!</p>
-        {/* {bronzesponsors.length>0 && <h3 className="mono text-peach hack mt-2">Bronze Sponsors</h3>}
+        }
+        {/* <h3 className="mono text-peach hack mt-5">Bronze Sponsors</h3> */}
+        {/* <p>Coming Soon!</p> */}
+        {bronzesponsors.length>0 && <h3 className="mono text-peach hack mt-2">Bronze Sponsors</h3>}
         {bronzesponsors.length>0 &&
         <Row className={`${!isMobile && "mt-1 mb-5"} d-flex justify-content-around`}>
           {bronzesponsors.map((sponsor) => 
@@ -118,9 +117,9 @@ function Sponsors() {
             </Col>
           )}
         </Row>
-        } */}
-        <h3 className="mono text-peach hack mt-5">Partners</h3>
-        <p>Coming Soon!</p>
+        }
+        {/* <h3 className="mono text-peach hack mt-5">Partners</h3>
+        <p>Coming Soon!</p> */}
         {/* {othersponsors.length>0 && <h3 className="mono text-peach hack mt-2">Partners</h3>}
         {othersponsors.length>0 &&
         <Row className={`${!isMobile && "mt-1 mb-5"} d-flex justify-content-around`}>
