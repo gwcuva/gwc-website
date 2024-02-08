fb9a869b2a95b7fbc9237e0883202724f19be280
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2024-02-07 20:24:11 -0500

    Lgz9cm/filx career fair title (#279)
    * change girls hoo code to girls who code
    
    * fixed some more "hoos" to "whos"

--- src/components/Main/Navbar/Navbar.tsx
+++ src/components/Main/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
     <nav className="bg-orange d-flex flex-row pr-2 pt-3 pb-3 pl-3 justify-content-between align-items-center">
       <img src={Logo} width="208px" height="82px" alt="Girls Who Code at the University of Virginia logo"/>
       <Row>
-      <a href="/careerfair" target="_blank" rel="noreferrer noopener" className="mono text-white interactive pr-5 pr-2">Girls Hoo Code Career Fair</a>
+      <a href="/careerfair" target="_blank" rel="noreferrer noopener" className="mono text-white interactive pr-5 pr-2">Girls Who Code Career Fair</a>
       <a href="/hack" target="_blank" rel="noreferrer noopener" className="mono text-white interactive pr-5 pr-2">Girls Hoo Hack</a>
       </Row>
   </nav>
