2f6b2d09d3d08804e2126d0ce36437043a8c79ad
VivianG <wvw4we@virginia.edu>
2024-09-27 15:08:28 -0400

    mostly fixed horizontal scroll bar issue, adding standout logo to footer

--- src/components/GirlsHooHack2024/Footer/Footer.tsx
+++ src/components/GirlsHooHack2024/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import Contact from '../../Main/Footer/Contact'
-import logo from '../../../assets/images/peacock-logo.svg';
+import GWCLogo from '../../../assets/images/peacock-logo.svg';
+import StandoutSticker from '../../../assets/images/stand-out-stickers-logo.png';
 import {Row, Col} from 'react-bootstrap';
 import {isMobile} from 'react-device-detect';
 
@@ -10,17 +11,20 @@ interface Props {
 
 function Footer(props: Props) {
   return (
-    <div id={props.id}>
+    <div id={props.id} style={{ overflow: 'hidden' }}>
       <div className="bg-turq d-flex flex-row pb-5 pt-5">
-        <Col md={isMobile ? 0 : 4} style={{textAlign:'center'}}>
-          <img src={logo} width={isMobile ? "112px" : "150px"} height={isMobile ? "44px" : "82px"} className={isMobile ? "pl-2" : "pl-5"} alt="Girls Who Code at the University of Virginia logo"/>
-        </Col>
         <Col md={10}>
           <Row>
+            <Col md={1} sm={2} className="text-white" style={{marginBottom:60}}>
+            </Col>
+            <Col md={isMobile ? 0 : 4} style={{textAlign:'center'}}>
+              <img src={GWCLogo} width={isMobile ? "112px" : "150px"} height={isMobile ? "44px" : "82px"} className={isMobile ? "pl-2" : "pl-5"} alt="Girls Who Code at the University of Virginia logo"/>
+              <img src={StandoutSticker} width={isMobile ? "75px" : "125px"} height={isMobile ? "40px" : "100px"} className={isMobile ? "pl-2" : "pl-5"} alt="Standout logo"/>
+            </Col>
             <Col md={2} sm={10} className="text-white" style={{marginBottom:60}}>
               <Contact />
             </Col>
-            <Col md={3} sm={4} className="text-white" style={{marginBottom:60}}>
+            <Col md={2} sm={2} className="text-white" style={{marginBottom:60}}>
             </Col>
             <Col md={3} sm={2} className="text-white">
               <text> We abide by the MLH Code of Conduct. Although this organization has members who are University of 
--- src/components/GirlsHooHack2024/NavbarGHH/NavbarGHH.tsx
+++ src/components/GirlsHooHack2024/NavbarGHH/NavbarGHH.tsx
@@ -32,11 +32,13 @@ function NavbarGHH() {
   }, []);
 
   return (
-    <nav className="bg-transparent d-flex flex-wrap justify-content-between align-items-center position Navbar"
-    style={{position: 'absolute', top: '2em', width: '100%', zIndex: 1000}}>
-      <img src={Logo} width="208px" height="60px" alt="Girls Who Code at the University of Virginia logo"/>
+    <nav className="bg-transparent d-flex flex-wrap justify-content-between align-items-center position Navbar" 
+    style={{position: 'absolute', top: 0, width: '100%', zIndex: 1000, overflow: 'hidden'}}>
       <Row>
-      <div className = "flex-row flex-wrap d-flex navbaradjustments">
+      <div className = "flex-row flex-wrap d-flex navbaradjustments"  style={{ marginTop: '0px', width: '100%' }}>
+      <img src={Logo} style={{ marginTop: '1%'}} width="208px" height="60px" alt="Girls Who Code at the University of Virginia logo"/>
+
+      <div style={{ marginTop: '1%', paddingRight: '2%' }}>
         <a href="#About" className="navbartext" >About</a>
         <a href="#Jumpstart" className="navbartext" >Jumpstart</a>
         <a href="#Schedule" className="navbartext">Schedule</a>
@@ -46,11 +48,12 @@ function NavbarGHH() {
         <a href="#FAQ" className="navbartext">FAQ</a>
         <a href="#Sponsors" className="navbartext">Sponsors</a>
         <a href="#SponsorshipProspectus" className="navbartext">Sponsorship</a>
-        <a href="#Newsletter" className="navbartext">Newsletter</a>
-      </div>
-      <div className="flex-row adjustments">
-          <a href={navbar.registration} target="_blank" rel="noreferrer noopener">
-            <img className="signupbanner pr-2" src={SignupBanner} alt="Signup banner" width="80" height="155"
+        <a href="#Newsletter" className="navbartext" >Newsletter</a>
+        </div>
+
+        <div className="flex-row overflow-hidden">
+          <a href={navbar.registration} target="_blank" rel="noreferrer noopener" style={{paddingRight: '5px' }}>
+            <img className="signupbanner pr-2" src={SignupBanner} alt="Signup banner" width="70" height="155"
             onMouseOver={e => (e.currentTarget.src = SignupBannerOrange)}
             onMouseOut={e => (e.currentTarget.src = SignupBanner)}
             />
@@ -59,6 +62,8 @@ function NavbarGHH() {
             <img className="mlhbanner" src={navbar.mlhbanner.url} alt="Major League Hacking 2024 Hackathon Season" width="85" height="170"/> 
           </a>
         </div>
+      </div>
+      
       </Row>
   </nav>
   );
